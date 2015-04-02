3c3f9bdf4aa1c5f72e9b465a5c65a098c15fcae4
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2015-04-02 15:59:17 +0100

    Updating for SpiNNMan version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="SpiNNFrontEndCommon",
-    version="2015.001-rc-01",
+    version="2015.001-rc-02",
     description="Common Spinnaker Front end functions",
     url="https://github.com/SpiNNakerManchester/SpiNNFrontEndCommon",
     packages=['spinn_front_end_common',
@@ -13,7 +13,7 @@ setup(
               'spinn_front_end_common.utility_models'],
     package_data={'spinn_front_end_common.common_model_binaries': ['*.aplx']},
     install_requires=['SpiNNMachine >= 2015.003-rc-01',
-                      'SpiNNMan >= 2015.003-rc-01',
+                      'SpiNNMan >= 2015.003-rc-02',
                       'SpiNNaker_PACMAN >= 2015.003-rc-01',
                       'SpiNNaker_DataSpecification >= 2015.003-rc-01',
                       'numpy', 'six']
