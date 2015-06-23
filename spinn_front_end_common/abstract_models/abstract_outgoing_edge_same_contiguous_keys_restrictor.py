720e2d480879b346acc9c60969ef1195e6e910ce
alan-stokes <alan.barry.stokes@gmail.com>
2015-06-23 16:55:21 +0100

    added outgoing_partitions to graphs and tied them into the tool chain for pacman and spynnaker. Graph front end changes should follow:

--- spinn_front_end_common/abstract_models/abstract_outgoing_edge_same_contiguous_keys_restrictor.py
+++ spinn_front_end_common/abstract_models/abstract_outgoing_edge_same_contiguous_keys_restrictor.py
@@ -4,9 +4,6 @@ from spinn_front_end_common.abstract_models\
 from pacman.model.constraints.key_allocator_constraints\
     .key_allocator_contiguous_range_constraint \
     import KeyAllocatorContiguousRangeContraint
-from pacman.model.constraints.key_allocator_constraints\
-    .key_allocator_same_keys_constraint \
-    import KeyAllocatorSameKeysConstraint
 
 
 class AbstractOutgoingEdgeSameContiguousKeysRestrictor(
@@ -19,35 +16,16 @@ class AbstractOutgoingEdgeSameContiguousKeysRestrictor(
 
         AbstractProvidesOutgoingEdgeConstraints.__init__(self)
 
-        # The first partitioned edge of this population for any subvertex,
-        # indexed by the subvertex lo atom
-        self._first_partitioned_edge = dict()
-
-        # Set of partitioned edges that have already had constraints added,
-        # to avoid over processing
-        self._seen_partitioned_edges = set()
-
     def get_outgoing_edge_constraints(self, partitioned_edge, graph_mapper):
+        """
+        returns any outgoing contraints for edges
+        :param partitioned_edge:
+        :param graph_mapper:
+        :return: iterable of constraints
+        """
 
-        # Find the slice of the vertex
-        vertex_slice = graph_mapper.get_subvertex_slice(
-            partitioned_edge.pre_subvertex)
-
-        # Keep track of the first partitioned edge
+        # All populations that use this data spec need to make sure that
+        # all edges out of each sub population have keys that are contiguous
         constraints = list()
-        if vertex_slice.lo_atom not in self._first_partitioned_edge:
-            self._first_partitioned_edge[vertex_slice.lo_atom] = \
-                partitioned_edge
-        elif partitioned_edge not in self._seen_partitioned_edges:
-
-            # All populations that use this data spec need to make sure that
-            # all edges out of each sub population are given the same keys, and
-            # that those keys are contiguous
-            constraints.append(KeyAllocatorContiguousRangeContraint())
-            constraints.append(KeyAllocatorSameKeysConstraint(
-                self._first_partitioned_edge[vertex_slice.lo_atom]))
-
-        # Avoid processing this edge again
-        self._seen_partitioned_edges.add(partitioned_edge)
-
+        constraints.append(KeyAllocatorContiguousRangeContraint())
         return constraints
