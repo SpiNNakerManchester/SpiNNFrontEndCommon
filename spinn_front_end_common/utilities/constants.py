3afcfaa61b1b59ccc1749531f79a818e62f16542
alan-stokes <alan.barry.stokes@gmail.com>
2015-08-13 10:34:34 +0100

    added constant to spike source array and reload buffered vertex

--- spinn_front_end_common/utilities/constants.py
+++ spinn_front_end_common/utilities/constants.py
@@ -9,6 +9,10 @@ SDRAM_BASE_ADDR = 0x70000000
 MAX_SAFE_BINARY_SIZE = 28 * 1024
 MAX_POSSIBLE_BINARY_SIZE = 33 * 1024
 
+# mas size expected to be used by the reverse iptag multicast source
+# during buffered opperations
+MAX_SIZE_OF_BUFFERED_REGION_ON_CHIP = 1 * 1024 * 1024
+
 # The number of words in the AbstractDataSpecable basic setup information
 DATA_SPECABLE_BASIC_SETUP_INFO_N_WORDS = 4
 
