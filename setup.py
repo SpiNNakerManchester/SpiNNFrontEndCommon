abd085804aacb89b36ab9b75905dc24e172bd0c8
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2015-08-25 15:50:53 +0100

    Updating versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="SpiNNFrontEndCommon",
-    version="2015.002",
+    version="2015.002-rc-01",
     description="Common Spinnaker Front end functions",
     url="https://github.com/SpiNNakerManchester/SpiNNFrontEndCommon",
     packages=['spinn_front_end_common',
@@ -14,13 +14,13 @@ setup(
               'spinn_front_end_common.interface.buffer_management.storage_objects',
               'spinn_front_end_common.utilities',
               'spinn_front_end_common.utilities.database',
-              'spinn_front_end_common.utilities.reload',
               'spinn_front_end_common.utilities.notification_protocol',
+              'spinn_front_end_common.utilities.reload',
               'spinn_front_end_common.utility_models'],
     package_data={'spinn_front_end_common.common_model_binaries': ['*.aplx']},
-    install_requires=['SpiNNMachine >= 2015.004',
-                      'SpiNNMan >= 2015.004',
-                      'SpiNNaker_PACMAN >= 2015.004',
-                      'SpiNNaker_DataSpecification >= 2015.004',
+    install_requires=['SpiNNMachine == 2015.004-rc-01',
+                      'SpiNNMan == 2015.004-rc-01',
+                      'SpiNNaker_PACMAN == 2015.004-rc-01',
+                      'SpiNNaker_DataSpecification == 2015.004-rc-01',
                       'numpy', 'six']
 )
