9120212e0495b732d094ed96a5c182fce93102c4
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2015-08-25 19:19:18 +0100

    Added a close to avoid issue with repeat runs

--- spinn_front_end_common/interface/buffer_management/buffer_manager.py
+++ spinn_front_end_common/interface/buffer_management/buffer_manager.py
@@ -377,6 +377,7 @@ class BufferManager(object):
             sent_messages.send_stop_message()
             if self._report_states.transciever_report:
                 self._reload_buffer_file[(vertex, region)].close()
+                del self._reload_buffer_file[(vertex, region)]
 
         # If there are no more messages, turn off requests for more messages
         if not vertex.is_next_timestamp(region) and sent_messages.is_empty():
@@ -434,3 +435,6 @@ class BufferManager(object):
         """
         with self._thread_lock:
             self._finished = True
+        if self._report_states.transciever_report:
+            for buffer_file in self._reload_buffer_file.itervalues():
+                buffer_file.close()
