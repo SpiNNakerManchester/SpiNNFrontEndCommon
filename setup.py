313354e85f11d4d8ae14ae943a8f24550ce33785
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2015-09-02 07:59:55 +0100

    Updated version for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="SpiNNFrontEndCommon",
-    version="2015.002-rc-01",
+    version="2015.002",
     description="Common Spinnaker Front end functions",
     url="https://github.com/SpiNNakerManchester/SpiNNFrontEndCommon",
     packages=['spinn_front_end_common',
@@ -18,9 +18,9 @@ setup(
               'spinn_front_end_common.utilities.reload',
               'spinn_front_end_common.utility_models'],
     package_data={'spinn_front_end_common.common_model_binaries': ['*.aplx']},
-    install_requires=['SpiNNMachine == 2015.004-rc-01',
-                      'SpiNNMan == 2015.004-rc-01',
-                      'SpiNNaker_PACMAN == 2015.004-rc-01',
-                      'SpiNNaker_DataSpecification == 2015.004-rc-01',
+    install_requires=['SpiNNMachine == 2015.004',
+                      'SpiNNMan == 2015.004',
+                      'SpiNNaker_PACMAN == 2015.004',
+                      'SpiNNaker_DataSpecification == 2015.004',
                       'numpy', 'six']
 )
