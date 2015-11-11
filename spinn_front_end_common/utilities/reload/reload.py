ee938388224fb6dc26012940eec316ae49cda2e8
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2015-11-11 08:51:21 +0000

    Extra tidying

--- spinn_front_end_common/utilities/reload/reload.py
+++ spinn_front_end_common/utilities/reload/reload.py
@@ -1,6 +1,3 @@
-"""
-reload script for loading onto a amchien wtihout going through the mapper
-"""
 from spinn_front_end_common.interface.buffer_management.buffer_manager import \
     BufferManager
 from spinn_front_end_common.utilities.notification_protocol\
@@ -34,8 +31,8 @@ class Reload(object):
                                 load_data=True):
         """
 
-        :param reload_application_data_items:  the application data for each
-        core which needs data to be reloaded to work
+        :param reload_application_data_items:  the application data for each\
+                    core which needs data to be reloaded to work
         :param load_data: a boolean which will not reload if set to false.
         :return: None
         """
@@ -67,8 +64,7 @@ class Reload(object):
         progress.end()
 
     def reload_routes(self, routing_tables, app_id=30):
-        """
-        reloads a set of routing tables
+        """ Reloads a set of routing tables
         :param routing_tables: the routing tables which need to be reloaded
         for the application to run successfully
         :param app_id: the id used to distinquish this for other applications
@@ -79,9 +75,9 @@ class Reload(object):
     def reload_binaries(self, executable_targets, app_id=30):
         """
 
-        :param executable_targets:the executable targets which needs to
-        be loaded onto the machine
-        :param app_id: the id used to distinquish this for other applications
+        :param executable_targets:the executable targets which needs to\
+                    be loaded onto the machine
+        :param app_id: the id used to identify this application
         :return: None
         """
         self._spinnaker_interface.load_executable_images(executable_targets,
@@ -89,9 +85,9 @@ class Reload(object):
 
     def reload_tags(self, iptags, reverse_iptags):
         """
-        reloads the tags required to get the simualtion exeucting
-        :param iptags: the iptags from the preivous run
-        :param reverse_iptags: the reverse iptags from the preivous run
+        reloads the tags required to get the simulation executing
+        :param iptags: the iptags from the previous run
+        :param reverse_iptags: the reverse iptags from the previous run
         :return:
         """
         self._spinnaker_interface.load_iptags(iptags)
@@ -100,12 +96,12 @@ class Reload(object):
     def restart(self, executable_targets, runtime, time_scaling,
                 turn_off_machine=True, app_id=30):
         """
-        :param executable_targets: the executable targets which needs to
-        be loaded onto the machine
-        :param runtime: the amount of time this application is expected to run
-        for
+        :param executable_targets: the executable targets which needs to\
+                    be loaded onto the machine
+        :param runtime: the amount of time this application is expected to run\
+                    for
         :param time_scaling: the time scale factor for timing purposes
-        :param app_id: the id used to distinquish this for other applications
+        :param app_id: the id used to identify this application
         :return: None
         """
         self._buffer_manager.load_initial_buffers()
@@ -119,8 +115,7 @@ class Reload(object):
             self._spinnaker_interface._txrx.power_off_machine()
 
     def enable_buffer_manager(self, buffered_placements, buffered_tags):
-        """
-        enables the buffer manager with the placements and buffered tags
+        """ Enables the buffer manager with the placements and buffered tags
         :param buffered_placements: the placements which contain buffered\
                     vertices
         :param buffered_tags: the tags which contain buffered vertices
@@ -149,8 +144,7 @@ class Reload(object):
         self._notification_protocol.send_read_notification(database_path)
 
     def _execute_start_messages(self):
-        """
-        sends the start messages to the external devices which need them
+        """ Sends the start messages to the external devices which need them
         :return:
         """
         if self._notification_protocol is not None:
