f8ae07943e6086d9fb407b43491ca76b6409f02a
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2016-03-08 12:23:56 +0000

    Removed unnecessary argument and improved progress

--- spinn_front_end_common/interface/interface_functions/front_end_common_runtime_updater.py
+++ spinn_front_end_common/interface/interface_functions/front_end_common_runtime_updater.py
@@ -14,27 +14,34 @@ class FrontEndCommonRuntimeUpdater(object):
     """
 
     def __call__(
-            self, placements, txrx, no_sync_changes, app_id,
+            self, txrx, no_sync_changes, app_id,
             executable_targets, no_machine_timesteps, loaded_binaries_token):
 
-        progress_bar = ProgressBar(2, "Updating on chip's runtime")
+        progress_bar = ProgressBar(
+            executable_targets.total_processors * 2, "Updating run time")
 
         if not loaded_binaries_token:
             raise exceptions.ConfigurationException(
-                "The run time token is set to false, and therefore the runtime"
-                "updater cannot be ran yet.")
+                "The binaries must be loaded before the run time updater is"
+                " called")
 
         # check that the right number of processors are in sync0
-        processors_ready = \
-            txrx.get_core_state_count(app_id, CPUState.CPU_STATE_12)
+        processors_ready = txrx.get_core_state_count(
+            app_id, CPUState.CPU_STATE_12)
         total_processors = executable_targets.total_processors
         all_core_subsets = executable_targets.all_core_subsets
+        progress_bar.update(processors_ready)
+        last_n_ready = processors_ready
 
         # check that all cores are in the state CPU_STATE_12 which shows that
         # the core has received the new runtime
         while processors_ready != total_processors:
             unsuccessful_cores = helpful_functions.get_cores_not_in_state(
                 all_core_subsets, CPUState.CPU_STATE_12, txrx)
+
+            if len(unsuccessful_cores) == 0:
+                progress_bar.update(total_processors - last_n_ready)
+                break
 
             for (x, y, p) in unsuccessful_cores:
                 infinite_run = 0
@@ -57,7 +64,8 @@ class FrontEndCommonRuntimeUpdater(object):
 
             processors_ready = txrx.get_core_state_count(
                 app_id, CPUState.CPU_STATE_12)
-        progress_bar.update()
+            progress_bar.update(processors_ready - last_n_ready)
+            last_n_ready = processors_ready
 
         # reset the state to the old state so that it can be used by the
         # application runner code
@@ -66,12 +74,18 @@ class FrontEndCommonRuntimeUpdater(object):
         else:
             sync_state = CPUState.SYNC1
         processors_ready = txrx.get_core_state_count(app_id, sync_state)
+        last_n_ready = processors_ready
+        progress_bar.update(processors_ready)
 
-        # check that all cores are in the state CPU_STATE_12 which shows that
+        # check that all cores are in the sync state which shows that
         # the core has received the new runtime
         while processors_ready != total_processors:
             unsuccessful_cores = helpful_functions.get_cores_not_in_state(
                 all_core_subsets, sync_state, txrx)
+
+            if len(unsuccessful_cores) == 0:
+                progress_bar.update(total_processors - last_n_ready)
+                break
 
             for (x, y, p) in unsuccessful_cores:
                 data = struct.pack(
@@ -87,7 +101,8 @@ class FrontEndCommonRuntimeUpdater(object):
                     destination_chip_y=y), data=data))
 
             processors_ready = txrx.get_core_state_count(app_id, sync_state)
-        progress_bar.update()
+            progress_bar.update(processors_ready - last_n_ready)
+            last_n_ready = processors_ready
         progress_bar.end()
 
         return {'no_sync_changes': no_sync_changes}
