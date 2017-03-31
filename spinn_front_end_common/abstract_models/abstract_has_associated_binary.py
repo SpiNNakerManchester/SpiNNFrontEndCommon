bce9aa61872c6989cd51e13065329e0f4c4d0a1f
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-03-31 13:39:22 +0100

    Shifted declaration for clarity

--- spinn_front_end_common/abstract_models/abstract_has_associated_binary.py
+++ spinn_front_end_common/abstract_models/abstract_has_associated_binary.py
@@ -5,6 +5,8 @@ from spinn_utilities.abstract_base import AbstractBase, abstractmethod
 
 @add_metaclass(AbstractBase)
 class AbstractHasAssociatedBinary(object):
+    
+    __slots__ = ()
 
     @abstractmethod
     def get_binary_file_name(self):
@@ -20,4 +22,3 @@ class AbstractHasAssociatedBinary(object):
             :py:class:`spinn_front_end_common.utilities.utility_objs.executable_start_type.ExecutableStartType`
         """
 
-    __slots__ = ()
