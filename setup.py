331f6e98f74e719a1a55ad7d2317e5472ff4bf40
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-04-26 09:37:45 +0100

    Updated versions

--- setup.py
+++ setup.py
@@ -24,18 +24,18 @@ for dirname, dirnames, filenames in os.walk(main_package_dir):
 
 setup(
     name="SpiNNFrontEndCommon",
-    version="3.0.1",
+    version="1!4.0.0a1",
     description="Common Spinnaker Front end functions",
     url="https://github.com/SpiNNakerManchester/SpiNNFrontEndCommon",
     packages=packages,
     package_data=package_data,
-    install_requires=['SpiNNUtilities >= 3.0.0, < 4.0.0',
-                      'SpiNNMachine >= 3.0.0, < 4.0.0',
-                      'SpiNNMan >= 3.0.0, < 4.0.0',
-                      'SpiNNaker_PACMAN >= 3.0.0, < 4.0.0',
-                      'SpiNNaker_DataSpecification >= 3.0.0, < 4.0.0',
-                      'SpiNNStorageHandlers >= 3.0.0, < 4.0.0',
-                      'spalloc >= v0.2.2, < v1.0.0',
+    install_requires=['SpiNNUtilities >= 1!4.0.0a1, < 1!5.0.0',
+                      'SpiNNMachine >= 1!4.0.0a1, < 1!5.0.0',
+                      'SpiNNMan >= 1!4.0.0a1, < 1!5.0.0',
+                      'SpiNNaker_PACMAN >= 1!4.0.0a1, < 1!5.0.0',
+                      'SpiNNaker_DataSpecification >= 1!4.0.0a1, < 1!5.0.0',
+                      'SpiNNStorageHandlers >= 1!4.0.0a1, < 1!5.0.0',
+                      'spalloc >= 0.2.2, < 1.0.0',
                       'requests >= 2.4.1',
                       'numpy', 'six']
 )
