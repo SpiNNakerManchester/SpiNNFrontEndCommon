fbf7c0431ca6bbadf91a196ee6067de6a1ed27b8
alan-stokes <alan.barry.stokes@gmail.com>
2017-05-26 11:39:08 +0100

    started algortihm holders

---
+++ spinn_front_end_common/interface/interface_functions/front_end_common_insert_edges_to_live_packet_gatherers.py
@@ -0,0 +1,6 @@
+
+class FrontEndCommonInsertEdgesToLivePacketGatherers(object):
+
+    def __call__(self, live_packet_gatherers, machine, machine_graph,
+                 application_graph=None, graph_mapper=None):
+
---
+++ spinn_front_end_common/interface/interface_functions/front_end_common_insert_live_packet_gatherers_to_graphs.py
@@ -0,0 +1,7 @@
+
+
+class FrontEndCommonInsertLivePacketGatherersToGraphs(object):
+
+    def __call__(
+            self, live_packet_gatherers, machine, machine_graph=None,
+            application_graph=None):
