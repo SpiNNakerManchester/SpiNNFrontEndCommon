2559a99dd48420b4d06433180106bf33c9d3aaff
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-07-20 15:11:06 +0100

    Improve clarity of spalloc machine probe.

--- spinn_front_end_common/interface/interface_functions/spalloc_max_machine_generator.py
+++ spinn_front_end_common/interface/interface_functions/spalloc_max_machine_generator.py
@@ -10,37 +10,40 @@ class SpallocMaxMachineGenerator(object):
 
     def __call__(
             self, spalloc_server, spalloc_port=22244, spalloc_machine=None):
-
-        client = ProtocolClient(spalloc_server, spalloc_port)
-        client.connect()
-        machines = client.list_machines()
-
+        with ProtocolClient(spalloc_server, spalloc_port) as client:
+            machines = client.list_machines()
+            # Close the context immediately; don't want to keep this particular
+            # connection around as there's not a great chance of this code
+            # being rerun in this process any time soon.
         max_width = None
         max_height = None
 
-        for machine in machines:
-            if ((spalloc_machine is None and "default" in machine["tags"]) or
-                    machine["name"] == spalloc_machine):
+        for machine in self._filter(machines, spalloc_machine):
+            # Get the width and height in chips
+            width = machine["width"] * 12
+            height = machine["height"] * 12
 
-                # Get the width and height in chips
-                width = machine["width"] * 12
-                height = machine["height"] * 12
+            # A specific exception is the 1-board machine, which is represented
+            # as a 3 board machine with 2 dead boards. In this case the width
+            # and height is 8.
+            if (machine["width"] == 1 and machine["height"] == 1
+                    and len(machine["dead_boards"]) == 2):
+                width = 8
+                height = 8
 
-                # A specific exception is the 1-board machine, which is
-                # represented as a 3 board machine with 2 dead boards.
-                # In this case the width and height is 8
-                if (machine["width"] == 1 and
-                        machine["height"] == 1 and
-                        len(machine["dead_boards"]) == 2):
-                    width = 8
-                    height = 8
+            # The "biggest" board is the one with the most chips
+            if ((max_width is None and max_height is None) or
+                    (width * height > max_width * max_height)):
+                max_width = width
+                max_height = height
 
-                # The "biggest" board is the one with the most chips
-                if ((max_width is None and max_height is None) or
-                        ((width * height) > (max_width * max_height))):
-                    max_width = width
-                    max_height = height
-
-        # Return the width and height, and make no assumption about
-        # wrap arounds or version
+        # Return the width and height, and make no assumption about wrap-
+        # arounds or version.
         return max_width, max_height, None, None
+
+    @staticmethod
+    def _filter(machines, filter):  # @ReservedAssignment
+        if filter is None:
+            return (m for m in machines if "default" in m["tags"])
+        else:
+            return (m for m in machines if m["name"] == filter)
