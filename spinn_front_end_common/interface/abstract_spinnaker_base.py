d4a4b0d6f5deed8e64860d7794b788375271b9e8
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-11-30 14:26:42 +0000

    Removed memory IO

--- spinn_front_end_common/interface/abstract_spinnaker_base.py
+++ spinn_front_end_common/interface/abstract_spinnaker_base.py
@@ -1785,8 +1785,6 @@ class AbstractSpinnakerBase(SimulatorInterface):
         optional_algorithms = list()
         optional_algorithms.append("RoutingTableLoader")
         optional_algorithms.append("TagsLoader")
-        optional_algorithms.append("WriteMemoryIOData")
-
         if self._exec_dse_on_host:
             optional_algorithms.append("HostExecuteDataSpecification")
         else:
