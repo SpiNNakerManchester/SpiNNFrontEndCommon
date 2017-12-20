002f3665dbc6e3d66354d3d4fe153fc58de01729
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-12-20 16:56:44 +0000

    Add comments

--- spinn_front_end_common/interface/interface_functions/write_memory_io_data.py
+++ spinn_front_end_common/interface/interface_functions/write_memory_io_data.py
@@ -12,7 +12,12 @@ import os
 
 
 class WriteMemoryIOData(object):
+    """ An algorithm that handles objects implementing the interface\
+        AbstractUsesMemoryIO
+    """
+
     __slots__ = [
+
         # The next tag to use by chip
         "_next_tag"
     ]
@@ -24,6 +29,21 @@ class WriteMemoryIOData(object):
             self, graph, placements, app_id, app_data_runtime_folder, hostname,
             transceiver=None, graph_mapper=None,
             processor_to_app_data_base_address=None):
+        """
+
+        :param graph: The graph to process
+        :param placements: The placements of vertices of the graph
+        :param app_id: The id of the application
+        :param app_data_runtime_folder: The location of data files
+        :param hostname: The host name of the machine
+        :param transceiver:\
+            The transceiver to write data using; if None only data files\
+            are written
+        :param graph_mapper: The optional mapping between graphs
+        :param processor_to_app_data_base_address:\
+            Optional existing dictionary of processor to base address
+        :return: The mapping between processor and addresses allocated
+        """
 
         if processor_to_app_data_base_address is None:
             processor_to_app_data_base_address = dict()
@@ -48,10 +68,25 @@ class WriteMemoryIOData(object):
         return processor_to_app_data_base_address
 
     def _get_used_tags(self, transceiver, x, y, heap_address):
+        """ Get the tags that have already been used on the given chip
+
+        :param transceiver: The transceiver to use to get the data
+        :param x: The x-coordinate of the chip
+        :param y: The y-coordinate of the chip
+        :param heap_address: The address of the heap to query for tags
+        :return: A tuple of used tags
+        """
         heap = transceiver.get_heap(x, y, heap=heap_address)
         return (element.tag for element in heap if not element.is_free)
 
     def _get_next_tag(self, transceiver, x, y):
+        """ Get the next SDRAM tag to use for the Memory IO on a given chip
+
+        :param transceiver: The transceiver to use to query for used tags
+        :param x: The x-coordinate of the chip
+        :param y: The y-coordinate of the chip
+        :return: The next available tag
+        """
         xy = (x, y)
         if xy not in self._next_tag:
             # Find the maximum tag already in use across the three areas
@@ -68,6 +103,20 @@ class WriteMemoryIOData(object):
     def _write_data_for_vertex(
             self, transceiver, placement, vertex, app_id,
             app_data_runtime_folder, hostname, base_address_map):
+        """ Write the data for the given vertex, if it supports the interface
+
+
+        :param transceiver:\
+            The transceiver to write data using; if None only data files\
+            are written
+        :param placement: The placement of the machine vertex
+        :param vertex:\
+            The vertex to write data for (might be an application vertex)
+        :param app_id: The id of the application
+        :param app_data_runtime_folder: The location of data files
+        :param hostname: The host name of the machine
+        :param base_address_map: Dictionary of processor to base address
+        """
         if isinstance(vertex, AbstractUsesMemoryIO):
             size = vertex.get_memory_io_data_size()
             if transceiver is not None:
