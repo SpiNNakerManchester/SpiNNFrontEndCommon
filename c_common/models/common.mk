dcf8513a7eb234e4fb917c4e75087243ce24a855
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2018-04-10 17:00:28 +0100

    all local files with src

--- c_common/models/common.mk
+++ c_common/models/common.mk
@@ -3,8 +3,3 @@ CURRENT_DIR := $(dir $(MAKEFILE_PATH))
 APP_OUTPUT_DIR := $(abspath $(CURRENT_DIR)../../spinn_front_end_common/common_model_binaries/)/
 
 include $(SPINN_DIRS)/make/local.mk
-
-test:
-	echo $(OBJECTS)
-	echo $(CURRENT_DIR)
-	echo $(MAKEFILE_LIST)
