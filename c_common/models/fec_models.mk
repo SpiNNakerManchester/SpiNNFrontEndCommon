f8ec0c537a1d667ab8129581728b06364b1da408
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2018-04-26 14:12:50 +0100

    renamed common.mk as that name used too often

--- c_common/models/fec_models.mk
+++ c_common/models/fec_models.mk
@@ -3,3 +3,5 @@ CURRENT_DIR := $(dir $(MAKEFILE_PATH))
 APP_OUTPUT_DIR := $(abspath $(CURRENT_DIR)../../spinn_front_end_common/common_model_binaries/)/
 
 include $(SPINN_DIRS)/make/local.mk
+
+.PRECIOUS: $(MODIFIED_DIR)%.c $(LOG_DICT_FILE) $(BUILD_DIR)%.nm $(BUILD_DIR)%.elf $(BUILD_DIR)%.bin
