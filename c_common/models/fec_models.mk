90468c3e3822d2617e7dbf6da2670d2400800c8d
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2018-04-27 16:26:11 +0100

    added local.mk moved out of spinn_common

--- c_common/models/fec_models.mk
+++ c_common/models/fec_models.mk
@@ -2,6 +2,6 @@ MAKEFILE_PATH := $(abspath $(lastword $(MAKEFILE_LIST)))
 CURRENT_DIR := $(dir $(MAKEFILE_PATH))
 APP_OUTPUT_DIR := $(abspath $(CURRENT_DIR)../../spinn_front_end_common/common_model_binaries/)/
 
-include $(SPINN_DIRS)/make/local.mk
+include $(CURRENT_DIR)../front_end_common_lib/local.mk
 
 .PRECIOUS: $(MODIFIED_DIR)%.c $(LOG_DICT_FILE) $(BUILD_DIR)%.nm $(BUILD_DIR)%.elf $(BUILD_DIR)%.bin
