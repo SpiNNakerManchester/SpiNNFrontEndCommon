42e038523124c51a29d140d42820fec0374e0f61
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2018-05-01 09:55:49 +0100

    Divide operations more neatly.

--- spinn_front_end_common/interface/interface_functions/load_other_executable_images.py
+++ spinn_front_end_common/interface/interface_functions/load_other_executable_images.py
@@ -1,13 +1,12 @@
-from spinn_front_end_common.utilities.utility_objs import ExecutableType
+import logging
+from spinn_front_end_common.utilities.utility_objs.ExecutableType \
+    import SYSTEM
 from spinn_machine import CoreSubsets
 from spinn_utilities.progress_bar import ProgressBar
-from spinn_front_end_common.utilities import helpful_functions
-
-from spinnman.messages.scp.enums import Signal
-from spinnman.model.enums import CPUState
-
-# general imports
-import logging
+from spinnman.messages.scp.enums.Signal import START
+from spinnman.model.enums.CPUState import READY
+from spinn_front_end_common.utilities.helpful_functions \
+    import flood_fill_binary_to_spinnaker
 
 logger = logging.getLogger(__name__)
 
@@ -16,37 +15,39 @@ class LoadOtherExecutableImages(object):
     __slots__ = []
 
     def __call__(self, executable_targets, app_id, transceiver):
-        """ Go through the executable targets and load each binary to \
-            everywhere and then send a start request to the cores that \
+        """ Go through the executable targets and load each binary to\
+            everywhere and then send a start request to the cores that\
             actually use it
         """
 
         progress = ProgressBar(
             executable_targets.total_processors + 1 -
-            executable_targets.get_n_cores_for_executable_type(
-                ExecutableType.SYSTEM),
+            executable_targets.get_n_cores_for_executable_type(SYSTEM),
             "Loading application executables onto the machine")
 
-        # only load executables not of system type.
-        none_system_core_subsets = CoreSubsets()
-        executable_types = executable_targets.executable_types_in_binary_set()
-        for executable_type in executable_types:
-            if executable_type != ExecutableType.SYSTEM:
-                for binary in executable_targets.\
-                        get_binaries_of_executable_type(
-                            executable_type):
-                    progress.update(
-                        helpful_functions.flood_fill_binary_to_spinnaker(
-                            executable_targets, binary, transceiver, app_id))
-                    none_system_core_subsets.add_core_subsets(
-                        executable_targets.get_cores_for_binary(binary))
-
-        self._start_simulation(transceiver, app_id, none_system_core_subsets)
+        user_core_subsets = self._load_user_executables(
+            transceiver, app_id, executable_targets, progress)
         progress.update()
+        self._start_simulation(transceiver, app_id, user_core_subsets)
         progress.end()
 
     @staticmethod
-    def _start_simulation(txrx, app_id, none_system_core_subsets):
-        txrx.wait_for_cores_to_be_in_state(
-            none_system_core_subsets, app_id, [CPUState.READY])
-        txrx.send_signal(app_id, Signal.START)
+    def _load_user_executables(txrx, app_id, targets, progress=None):
+        user_core_subsets = CoreSubsets()
+        for exe_type in targets.executable_types_in_binary_set():
+            # only load executables not of system type
+            if exe_type == SYSTEM:
+                continue
+            for binary in targets.get_binaries_of_executable_type(exe_type):
+                n_loaded = flood_fill_binary_to_spinnaker(
+                    targets, binary, txrx, app_id)
+                if progress is not None:
+                    progress.update(n_loaded)
+                user_core_subsets.add_core_subsets(
+                    targets.get_cores_for_binary(binary))
+        return user_core_subsets
+
+    @staticmethod
+    def _start_simulation(txrx, app_id, user_core_subsets):
+        txrx.wait_for_cores_to_be_in_state(user_core_subsets, app_id, [READY])
+        txrx.send_signal(app_id, START)
--- spinn_front_end_common/interface/interface_functions/load_system_executable_images.py
+++ spinn_front_end_common/interface/interface_functions/load_system_executable_images.py
@@ -1,12 +1,11 @@
-from spinn_front_end_common.utilities import helpful_functions
-from spinn_front_end_common.utilities.utility_objs import ExecutableType
-from spinn_utilities.progress_bar import ProgressBar
-
-# general imports
 import logging
-
-from spinnman.messages.scp.enums import Signal
-from spinnman.model.enums import CPUState
+from spinn_front_end_common.utilities.helpful_functions \
+    import flood_fill_binary_to_spinnaker
+from spinn_front_end_common.utilities.utility_objs.ExecutableType \
+    import SYSTEM
+from spinn_utilities.progress_bar import ProgressBar
+from spinnman.messages.scp.enums.Signal import START
+from spinnman.model.enums.CPUState import READY
 
 logger = logging.getLogger(__name__)
 
@@ -15,23 +14,25 @@ class LoadSystemExecutableImages(object):
     __slots__ = []
 
     def __call__(self, executable_targets, app_id, transceiver):
-        """ Go through the executable targets and load each binary to \
-            everywhere and then send a start request to the cores that \
+        """ Go through the executable targets and load each binary to\
+            everywhere and then send a start request to the cores that\
             actually use it
         """
 
         progress = ProgressBar(
-            executable_targets.get_n_cores_for_executable_type(
-                ExecutableType.SYSTEM),
+            executable_targets.get_n_cores_for_executable_type(SYSTEM),
             "Loading system executables onto the machine")
-
-        for binary in executable_targets.get_binaries_of_executable_type(
-                ExecutableType.SYSTEM):
-            progress.update(
-                helpful_functions.flood_fill_binary_to_spinnaker(
-                    executable_targets, binary, transceiver, app_id))
-            transceiver.wait_for_cores_to_be_in_state(
-                executable_targets.get_cores_for_binary(binary), app_id,
-                [CPUState.READY])
-        transceiver.send_signal(app_id, Signal.START)
+        self._launch_system_executables(
+            transceiver, app_id, executable_targets, progress)
         progress.end()
+
+    @staticmethod
+    def _launch_system_executables(txrx, app_id, targets, progress=None):
+        for binary in targets.get_binaries_of_executable_type(SYSTEM):
+            n_loaded = flood_fill_binary_to_spinnaker(
+                targets, binary, txrx, app_id)
+            if progress is not None:
+                progress.update(n_loaded)
+            txrx.wait_for_cores_to_be_in_state(
+                targets.get_cores_for_binary(binary), app_id, [READY])
+        txrx.send_signal(app_id, START)
