a629e1e0fd4a1a712ece202452edabc45fdd52e8
alan-stokes <alan.barry.stokes@gmail.com>
2018-05-23 13:12:35 +0100

    removed debug and flake8

--- spinn_front_end_common/interface/interface_functions/graph_data_specification_writer.py
+++ spinn_front_end_common/interface/interface_functions/graph_data_specification_writer.py
@@ -112,47 +112,45 @@ class GraphDataSpecificationWriter(object):
         :return: True if the vertex was data specable, False otherwise
         """
         # pylint: disable=too-many-arguments
-        try:
-            # if the vertex can generate a DSG, call it
-            if not isinstance(vertex, AbstractGeneratesDataSpecification):
-                return False
+        # if the vertex can generate a DSG, call it
+        if not isinstance(vertex, AbstractGeneratesDataSpecification):
+            return False
 
-            # build the writers for the reports and data
-            data_writer_filename, spec = get_data_spec_and_file_writer_filename(
+        # build the writers for the reports and data
+        data_writer_filename, spec = \
+            get_data_spec_and_file_writer_filename(
                 placement.x, placement.y, placement.p, hostname,
                 report_default_directory,
                 write_text_specs, app_data_runtime_folder)
 
-            # link dsg file to vertex
-            dsg_targets[placement.x, placement.y, placement.p] = \
-                data_writer_filename
+        # link dsg file to vertex
+        dsg_targets[placement.x, placement.y, placement.p] = \
+            data_writer_filename
 
-            # generate the dsg file
-            vertex.generate_data_specification(spec, placement)
+        # generate the dsg file
+        vertex.generate_data_specification(spec, placement)
 
-            # Check the memory usage
-            self._region_sizes[placement.vertex] = spec.region_sizes
-            self._vertices_by_chip[placement.x, placement.y].append(
-                placement.vertex)
-            self._sdram_usage[placement.x, placement.y] += sum(
-                spec.region_sizes)
-            if (self._sdram_usage[placement.x, placement.y] <=
-                    machine.get_chip_at(placement.x, placement.y).sdram.size):
-                return True
+        # Check the memory usage
+        self._region_sizes[placement.vertex] = spec.region_sizes
+        self._vertices_by_chip[placement.x, placement.y].append(
+            placement.vertex)
+        self._sdram_usage[placement.x, placement.y] += sum(
+            spec.region_sizes)
+        if (self._sdram_usage[placement.x, placement.y] <=
+                machine.get_chip_at(placement.x, placement.y).sdram.size):
+            return True
 
-            # creating the error message which contains the memory usage of
-            #  what each core within the chip uses and its original
-            # estimate.
-            memory_usage = "\n".join((
-                "    {}: {} (total={}, estimated={})".format(
-                    vert, self._region_sizes[vert],
-                    sum(self._region_sizes[vert]),
-                    vert.resources_required.sdram.get_value())
-                for vert in self._vertices_by_chip[placement.x, placement.y]))
+        # creating the error message which contains the memory usage of
+        #  what each core within the chip uses and its original
+        # estimate.
+        memory_usage = "\n".join((
+            "    {}: {} (total={}, estimated={})".format(
+                vert, self._region_sizes[vert],
+                sum(self._region_sizes[vert]),
+                vert.resources_required.sdram.get_value())
+            for vert in self._vertices_by_chip[placement.x, placement.y]))
 
-            raise ConfigurationException(
-                "Too much SDRAM has been used on {}, {}.  Vertices and"
-                " their usage on that chip is as follows:\n{}".format(
-                    placement.x, placement.y, memory_usage))
-        except Exception as e:
-            print "a"
+        raise ConfigurationException(
+            "Too much SDRAM has been used on {}, {}.  Vertices and"
+            " their usage on that chip is as follows:\n{}".format(
+                placement.x, placement.y, memory_usage))
