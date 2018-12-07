d5fa8b8d693017b9b8bfc9cc8cea9817e7d82cd5
Andrew Gait <andrew.gait@manchester.ac.uk>
2018-12-07 10:41:46 +0000

    flake8 unused import

--- spinn_front_end_common/interface/interface_functions/insert_extra_monitor_vertices_to_graphs.py
+++ spinn_front_end_common/interface/interface_functions/insert_extra_monitor_vertices_to_graphs.py
@@ -1,7 +1,6 @@
 from spinn_utilities.progress_bar import ProgressBar
 from pacman.model.constraints.placer_constraints import ChipAndCoreConstraint
 from pacman.model.graphs.common import Slice
-from pacman.utilities.utility_calls import locate_constraints_of_type
 from spinn_front_end_common.utility_models import (
     DataSpeedUpPacketGather, DataSpeedUpPacketGatherMachineVertex,
     ExtraMonitorSupport, ExtraMonitorSupportMachineVertex)
