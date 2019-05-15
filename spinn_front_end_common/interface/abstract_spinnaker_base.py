708ff697c79654b92a1ae1cfc94aa1892bd4d554
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-05-15 14:44:37 +0100

    write_compressed_router_summary_report

--- spinn_front_end_common/interface/abstract_spinnaker_base.py
+++ spinn_front_end_common/interface/abstract_spinnaker_base.py
@@ -1471,6 +1471,9 @@ class AbstractSpinnakerBase(ConfigHandler, SimulatorInterface):
             if self._config.getboolean(
                     "Reports", "write_router_summary_report"):
                 algorithms.append("RouterSummaryReport")
+            if self._config.getboolean(
+                    "Reports", "write_compressed_router_summary_report"):
+                algorithms.append("CompressedRouterSummaryReport")
             if self._config.getboolean("Reports",
                                        "write_routing_table_reports"):
                 optional_algorithms.append("unCompressedRoutingTableReports")
