91f349d4731455e8298322e0be19d6674875a9bc
alan-stokes <alan.barry.stokes@gmail.com>
2019-05-23 09:36:20 +0100

    fixes bug with verts where the machine vertex doesnt have the binary type, but the app vertex does

--- spinn_front_end_common/mapping_algorithms/find_application_chips_used.py
+++ spinn_front_end_common/mapping_algorithms/find_application_chips_used.py
@@ -4,11 +4,22 @@ from spinn_front_end_common.utilities.utility_objs import ExecutableType
 
 class FindApplicationChipsUsed(object):
 
-    def __call__(self, placements):
+    def __call__(self, placements, graph_mapper=None):
         chips_used = set()
         for placement in placements:
-            if (isinstance(placement.vertex, AbstractHasAssociatedBinary) and
-                    placement.vertex.get_binary_start_type() !=
-                    ExecutableType.SYSTEM):
+            # find binary type if applicable
+            binary_start_type = None
+            has_binary = isinstance(
+                placement.vertex, AbstractHasAssociatedBinary)
+
+            if not has_binary and graph_mapper is not None:
+                app_vertex = graph_mapper.get_application_vertex(
+                    placement.vertex)
+                if isinstance(app_vertex, AbstractHasAssociatedBinary):
+                    binary_start_type = app_vertex.get_binary_start_type()
+            else:
+                binary_start_type = placement.vertex.get_binary_start_type()
+
+            if binary_start_type != ExecutableType.SYSTEM:
                 chips_used.add((placement.x, placement.y))
         return len(chips_used)
