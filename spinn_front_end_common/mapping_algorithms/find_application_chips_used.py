e0b76aedad99db385841ddc7f86e799e1b193aa1
alan-stokes <alan.barry.stokes@gmail.com>
2019-05-23 11:21:10 +0100

    more fixes to get mapping report to be complete and accurate

--- spinn_front_end_common/mapping_algorithms/find_application_chips_used.py
+++ spinn_front_end_common/mapping_algorithms/find_application_chips_used.py
@@ -1,4 +1,6 @@
 from spinn_front_end_common.abstract_models import AbstractHasAssociatedBinary
+from spinn_front_end_common.utilities.helpful_functions import \
+    find_executable_start_type
 from spinn_front_end_common.utilities.utility_objs import ExecutableType
 
 
@@ -8,17 +10,8 @@ class FindApplicationChipsUsed(object):
         chips_used = set()
         for placement in placements:
             # find binary type if applicable
-            binary_start_type = None
-            has_binary = isinstance(
-                placement.vertex, AbstractHasAssociatedBinary)
-
-            if not has_binary and graph_mapper is not None:
-                app_vertex = graph_mapper.get_application_vertex(
-                    placement.vertex)
-                if isinstance(app_vertex, AbstractHasAssociatedBinary):
-                    binary_start_type = app_vertex.get_binary_start_type()
-            else:
-                binary_start_type = placement.vertex.get_binary_start_type()
+            binary_start_type = find_executable_start_type(
+                placement.vertex, graph_mapper)
 
             if binary_start_type != ExecutableType.SYSTEM:
                 chips_used.add((placement.x, placement.y))
--- spinn_front_end_common/utilities/report_functions/bit_field_summary.py
+++ spinn_front_end_common/utilities/report_functions/bit_field_summary.py
@@ -2,10 +2,18 @@
 
 class BitFieldSummary(object):
 
-    def __init__(self, total_merged, max_per_chip, lowest_per_chip):
+    def __init__(
+            self, total_merged, max_per_chip, lowest_per_chip, total_to_merge,
+            max_to_merge_per_chip, low_to_merge_per_chip,
+            average_per_chip_merged, average_per_chip_to_merge):
         self._total_merged = total_merged
         self._max_per_chip = max_per_chip
         self._lowest_per_chip = lowest_per_chip
+        self._total_to_merge = total_to_merge
+        self._max_to_merge_per_chip = max_to_merge_per_chip
+        self._low_to_merge_per_chip = low_to_merge_per_chip
+        self._average_per_chip_merged = average_per_chip_merged
+        self._average_per_chip_to_merge = average_per_chip_to_merge
 
     @property
     def total_merged(self):
@@ -18,3 +26,24 @@ class BitFieldSummary(object):
     @property
     def lowest_per_chip(self):
         return self._lowest_per_chip
+
+    @property
+    def total_to_merge(self):
+        return self._total_to_merge
+
+    @property
+    def max_to_merge_per_chip(self):
+        return self._max_to_merge_per_chip
+
+    @property
+    def low_to_merge_per_chip(self):
+        return self._low_to_merge_per_chip
+
+    @property
+    def average_per_chip_merged(self):
+        return self._average_per_chip_merged
+
+    @property
+    def average_per_chip_to_merge(self):
+        return self._average_per_chip_to_merge
+
