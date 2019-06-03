ee6db01f40cd20a720c381faaf24b2ddced11b63
alan-stokes <alan.barry.stokes@gmail.com>
2019-06-03 10:32:39 +0100

    extra params

--- spinn_front_end_common/mapping_algorithms/find_application_chips_used.py
+++ spinn_front_end_common/mapping_algorithms/find_application_chips_used.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+
+import sys
+
 from spinn_front_end_common.utilities.helpful_functions import \
     find_executable_start_type
 from spinn_front_end_common.utilities.utility_objs import ExecutableType
@@ -6,12 +10,24 @@ from spinn_front_end_common.utilities.utility_objs import ExecutableType
 class FindApplicationChipsUsed(object):
 
     def __call__(self, placements, graph_mapper=None):
-        chips_used = set()
+        chips_used = defaultdict(int)
         for placement in placements:
             # find binary type if applicable
             binary_start_type = find_executable_start_type(
                 placement.vertex, graph_mapper)
 
             if binary_start_type != ExecutableType.SYSTEM:
-                chips_used.add((placement.x, placement.y))
-        return len(chips_used)
+                chips_used[(placement.x, placement.y)] += 1
+
+        low = sys.maxint
+        high = 0
+        average = 0
+
+        for key in chips_used:
+            if chips_used[key] < low:
+                low = chips_used[key]
+            if chips_used[key] > high:
+                high = chips_used[key]
+            average += chips_used[key]
+        average = average / len(chips_used.keys())
+        return len(chips_used.keys()), high, low, average
