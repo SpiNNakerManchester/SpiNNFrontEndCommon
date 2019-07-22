a210598bb395a3e0fb5c5706a13048aa4774a692
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-07-22 13:51:42 +0100

    quicksort

--- c_common/models/minimise/src/simple_minimise.c
+++ c_common/models/minimise/src/simple_minimise.c
@@ -87,16 +87,84 @@ static inline void compress_by_route(table_t *table, uint32_t left, uint32_t rig
     }
 }
 
+static inline void swap(table_t *table, uint32_t a, uint32_t b){
+    log_info("swap %u %u", a, b);
+    entry_t temp = table->entries[a];
+    table->entries[a] = table->entries[b];
+    table->entries[b] = temp;
+}
+
+static void quicksort(table_t *table, uint32_t low, uint32_t high){
+    // Sorts the entries in place based on route
+    // param low: Inclusive lowest index to consider
+    // param high: Inclusive highest index to consider
+    for (uint32_t i = 0; i < table->size; i++) {
+        entry_t entry = table->entries[i];
+        log_info("entry %u %u %u %u %u", i, entry.keymask.key, entry.keymask.mask, entry.route, entry.source);
+    }
+    log_info("sort %u %u", low, high);
+    if (low < high) {
+        uint32_t right = low + 1;
+        uint32_t left = low;
+        uint32_t h_write = high;
+        log_info("pre sort %u %u %u %u %u" , low, left, right, h_write, high);
+        entry_t entry = table->entries[left];
+        log_info("entry %u %u %u %u %u", left, entry.keymask.key, entry.keymask.mask, entry.route, entry.source);
+        uint32_t pivot = table->entries[left].route;
+        log_info("A sort %u %u %u %u %u %u", low, left, right, h_write, high, pivot);
+        while (right <= h_write){
+            if (table->entries[right].route < pivot){
+                swap(table, left, right);
+                left++;
+                right++;
+                log_info("B sort %u %u %u %u %u %u", low, left, right, h_write, high, pivot);
+                for (uint32_t i = 0; i < table->size; i++) {
+                    entry_t entry = table->entries[i];
+                    log_info("entry %u %u %u %u %u", i, entry.keymask.key, entry.keymask.mask, entry.route, entry.source);
+                }
+            } else if (table->entries[right].route > pivot) {
+                swap(table, h_write, right);
+                h_write--;
+                log_info("C sort %u %u %u %u %u %u", low, left, right, h_write, high, pivot);
+                for (uint32_t i = 0; i < table->size; i++) {
+                    entry_t entry = table->entries[i];
+                    log_info("entry %u %u %u %u %u", i, entry.keymask.key, entry.keymask.mask, entry.route, entry.source);
+                }
+            } else {
+                right++;
+                log_info("D sort %u %u %u %u %u %u", low, left, right, h_write, high, pivot);
+                for (uint32_t i = 0; i < table->size; i++) {
+                    entry_t entry = table->entries[i];
+                    log_info("entry %u %u %u %u %u", i, entry.keymask.key, entry.keymask.mask, entry.route, entry.source);
+                }
+            }
+        }
+        log_info("end sort %u %u %u %u %u", low, left, right, h_write, high);
+        quicksort(table, low, left - 1);
+        quicksort(table, right, high);
+    }
+    for (uint32_t i = 0; i < table->size; i++) {
+        entry_t entry = table->entries[i];
+        log_info("entry %u %u %u %u %u", i, entry.keymask.key, entry.keymask.mask, entry.route, entry.source);
+    }
+}
+
 static inline void simple_minimise(table_t *table, uint32_t target_length){
     uint32_t left, right;
 
-    log_info("do qsort by route");
-    qsort(table->entries, table->size, sizeof(entry_t), compare_rte_by_route);
+    for (uint32_t i = 0; i < table->size; i++) {
+        entry_t entry = table->entries[i];
+        log_info("entry %u %u %u %u %u", i, entry.keymask.key, entry.keymask.mask, entry.route, entry.source);
+    }
 
-    log_info("doing sort");
-    write_index = 0;
-    previous_index = 0;
-    max_index = table->size - 1;
+    log_info("do qsort by route");
+    quicksort(table, 0, table->size-1);
+
+    return;
+
+    uint32_t write_index = 0;
+    uint32_t previous_index = 0;
+    uint32_t max_index = table->size - 1;
     left = 0;
 
     while (left < table->size){
