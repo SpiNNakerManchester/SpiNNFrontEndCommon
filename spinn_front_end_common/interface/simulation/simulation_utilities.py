54607874f919135e71e5828a63f404e28d97b021
alan-stokes <alan.barry.stokes@gmail.com>
2019-07-24 11:46:14 +0100

    supports different timer periods.

--- spinn_front_end_common/interface/simulation/simulation_utilities.py
+++ spinn_front_end_common/interface/simulation/simulation_utilities.py
@@ -19,12 +19,14 @@ from spinn_front_end_common.utilities.constants import SDP_PORTS
 
 
 def get_simulation_header_array(
-        binary_file_name, machine_time_step, time_scale_factor):
+        binary_file_name, machine_time_step, time_scale_factor,
+        timer_period=None):
     """ Get data to be written to the simulation header
 
     :param binary_file_name: The name of the binary of the application
     :param machine_time_step: The time step of the simulation
     :param time_scale_factor: The time scaling of the simulation
+    :param timer_period: settable if not using default
     :return: An array of values to be written as the simulation header
     """
     # Get first 32-bits of the md5 hash of the application name
@@ -33,7 +35,12 @@ def get_simulation_header_array(
     # Write this to the system region (to be picked up by the simulation):
     data = list()
     data.append(int(application_name_hash, 16))
-    data.append(machine_time_step * time_scale_factor)
+
+    # set timer period accordingly
+    if timer_period is None:
+        data.append(machine_time_step * time_scale_factor)
+    else:
+        data.append(timer_period)
 
     # add SDP port number for receiving synchronisations and new run times
     data.append(SDP_PORTS.RUNNING_COMMAND_SDP_PORT.value)
