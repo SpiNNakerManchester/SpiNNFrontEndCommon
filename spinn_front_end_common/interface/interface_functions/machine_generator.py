1acd4fc48c2fb0ae84a170dae4c3556134b52d17
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-11-12 14:16:58 +0000

    default_report_directory

--- spinn_front_end_common/interface/interface_functions/machine_generator.py
+++ spinn_front_end_common/interface/interface_functions/machine_generator.py
@@ -31,7 +31,7 @@ class MachineGenerator(object):
             downed_links, board_version, auto_detect_bmp,
             scamp_connection_data, boot_port_num, reset_machine_on_start_up,
             max_sdram_size=None, repair_machine=False,
-            ignore_bad_ethernets=True):
+            ignore_bad_ethernets=True, default_report_directory=None):
         """
         :param hostname: the hostname or IP address of the SpiNNaker machine
         :param bmp_details: the details of the BMP connections
@@ -88,7 +88,8 @@ class MachineGenerator(object):
             scamp_connections=scamp_connection_data,
             max_sdram_size=max_sdram_size,
             repair_machine=repair_machine,
-            ignore_bad_ethernets=ignore_bad_ethernets)
+            ignore_bad_ethernets=ignore_bad_ethernets,
+            default_report_directory=default_report_directory)
 
         if reset_machine_on_start_up:
             txrx.power_off_machine()
