ad6fb6cc2e3dcc65c6c8c0bf3debde4e282a575f
alan-stokes <alan.barry.stokes@gmail.com>
2019-11-21 16:18:27 +0000

    p8

--- spinn_front_end_common/interface/interface_functions/host_execute_data_specification.py
+++ spinn_front_end_common/interface/interface_functions/host_execute_data_specification.py
@@ -14,6 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from collections import OrderedDict
+
 import logging
 import struct
 import numpy
@@ -22,8 +23,7 @@ from spinn_utilities.progress_bar import ProgressBar
 from spinn_utilities.log import FormatAdapter
 from spinn_machine import CoreSubsets
 from data_specification import DataSpecificationExecutor
-from data_specification.constants import MAX_MEM_REGIONS, \
-    APP_PTR_TABLE_BYTE_SIZE
+from data_specification.constants import MAX_MEM_REGIONS
 from data_specification.exceptions import DataSpecificationException
 from spinn_front_end_common.interface.ds.ds_write_info import DsWriteInfo
 from spinn_front_end_common.utilities.helpful_functions import (
