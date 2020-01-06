6747fbf1a8afd77abd114867b7dd3694442c5dda
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2020-01-06 10:46:20 +0000

    flake8

--- spinn_front_end_common/abstract_models/application_spinnaker_link_vertex.py
+++ spinn_front_end_common/abstract_models/application_spinnaker_link_vertex.py
@@ -38,8 +38,7 @@ class ApplicationSpiNNakerLinkVertex(
 
     def __init__(
             self, n_atoms, spinnaker_link_id, board_address=None, label=None,
-            constraints=None, max_atoms_per_core=sys.maxsize,
-            timestep_in_us=None):
+            constraints=None, max_atoms_per_core=sys.maxsize):
         super(ApplicationSpiNNakerLinkVertex, self).__init__(
             label=label, constraints=constraints,
             max_atoms_per_core=max_atoms_per_core)
