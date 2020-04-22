062d1242ce49e368676bd06987fc19decf5e42a3
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2020-04-22 09:51:42 +0100

    Vera

--- c_common/models/minimise/src/unordered_remove_default_routes.h
+++ c_common/models/minimise/src/unordered_remove_default_routes.h
@@ -26,22 +26,25 @@
 #ifndef __REMOVE_DEFAULT_ROUTES_H__
 #define __REMOVE_DEFAULT_ROUTES_H__
 
-static inline bool _just_a_link(uint32_t direction)
-{
+//! Picks the bits of a link out of a route
+#define LINK_MASK 0x3f
+
+static inline bool _just_a_link(uint32_t direction) {
     return __builtin_popcount(direction) == 1 && // Only one direction...
-            (direction & 0x3f);                  // which is a link.
+            (direction & LINK_MASK);             // which is a link.
 }
 
+// Test if route's source is opposite to sink
 static inline bool _opposite_links(entry_t *entry) {
-    return
-        (entry->route >> 3) == (entry->source & 0x7) && // Source is opposite to sink
-        (entry->source >> 3) == (entry->route & 0x7);   // Sink is opposite to source
+    uint32_t src = entry->source & LINK_MASK;
+    uint32_t dst = entry->route & LINK_MASK;
+    // Equivalent to a rotate of 6 bits by 3 places and single compare
+    return ((dst >> 3) == (src & 0x7) && (src >> 3) == (dst & 0x7));
 }
 
 //! \brief Removes defaultable routes from a routing table if that helps.
 //! \param[in,out] table: The table to remove the routes from.
-static inline void remove_default_routes_minimise(table_t *table)
-{
+static inline void remove_default_routes_minimise(table_t *table) {
     // Work out if removing defaultable links is worthwhile
     uint32_t after_size = table->size;
     for (uint32_t i = 0; i < table->size; i++) {
