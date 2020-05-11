d667e9401a311e86e233e4ed6c5ed535dc212fff
Andrew Gait <andrew.gait@manchester.ac.uk>
2020-05-11 10:06:04 +0100

    Solve the issue using tokens instead

--- spinn_front_end_common/interface/abstract_spinnaker_base.py
+++ spinn_front_end_common/interface/abstract_spinnaker_base.py
@@ -1974,8 +1974,7 @@ class AbstractSpinnakerBase(ConfigHandler, SimulatorInterface):
             algorithms.append("SdramUsageReportPerChip")
 
         # Clear iobuf from machine
-        if (self._has_ran and
-            not self._use_virtual_board and not self._empty_graphs and
+        if (not self._use_virtual_board and not self._empty_graphs and
                 self._config.getboolean("Reports", "clear_iobuf_during_run")):
             algorithms.append("ChipIOBufClearer")
 
