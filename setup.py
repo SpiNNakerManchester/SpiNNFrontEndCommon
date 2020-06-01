5e461f490daef76195e3a408b90b64211e71c445
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2020-06-01 16:58:22 +0100

    Extra magic so that tools actually gets installed right...

--- setup.py
+++ setup.py
@@ -22,24 +22,26 @@ exec(open("spinn_front_end_common/_version.py").read())
 assert __version__
 
 # Build a list of all project modules, as well as supplementary files
-main_package = "spinn_front_end_common"
+main_packages = ("spinn_front_end_common", "tools")
 extensions = {".aplx", ".boot", ".cfg", ".json", ".sql", ".template", ".xml",
-              ".xsd", ".dict"}
-main_package_dir = os.path.join(os.path.dirname(__file__), main_package)
-start = len(main_package_dir)
+              ".xsd", ".dict", ".struct", ".conf"}
+
 packages = []
 package_data = defaultdict(list)
-for dirname, dirnames, filenames in os.walk(main_package_dir):
-    if '__init__.py' in filenames:
-        package = "{}{}".format(
-            main_package, dirname[start:].replace(os.sep, '.'))
-        packages.append(package)
-    for filename in filenames:
-        _, ext = os.path.splitext(filename)
-        if ext in extensions:
+for main_package in main_packages:
+    main_package_dir = os.path.join(os.path.dirname(__file__), main_package)
+    start = len(main_package_dir)
+    for dirname, dirnames, filenames in os.walk(main_package_dir):
+        if '__init__.py' in filenames:
             package = "{}{}".format(
                 main_package, dirname[start:].replace(os.sep, '.'))
-            package_data[package].append(filename)
+            packages.append(package)
+        for filename in filenames:
+            _, ext = os.path.splitext(filename)
+            if ext in extensions:
+                package = "{}{}".format(
+                    main_package, dirname[start:].replace(os.sep, '.'))
+                package_data[package].append(filename)
 
 setup(
     name="SpiNNFrontEndCommon",
