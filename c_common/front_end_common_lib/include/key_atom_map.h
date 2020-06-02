1a0b7500189012371d9577ebe09ab91841e89f18
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2020-06-02 10:08:17 +0100

    FEC lib doc comment & cleanup

--- c_common/front_end_common_lib/include/key_atom_map.h
+++ c_common/front_end_common_lib/include/key_atom_map.h
@@ -18,18 +18,18 @@
 #ifndef __KEY_ATOM_MAP_H__
 
 //! \brief struct for key and atoms
-typedef struct key_atom_pair_t{
-    // key
+typedef struct key_atom_pair_t {
+    //! key
     uint32_t key;
-    // n atoms
+    //! number of atoms for the key
     int n_atoms;
 } key_atom_pair_t;
 
 //! \brief key atom map struct
 typedef struct key_atom_data_t{
-    // how many key atom maps
+    //! how many key-atom maps
     int n_pairs;
-    // the list of maps
+    //! the array of maps
     key_atom_pair_t pairs[];
 } key_atom_data_t;
 
