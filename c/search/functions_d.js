9bb50b7be3517d7b551e770de405b8321a4d0d0c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-17 09:17:40 +0000

    Deploy SpiNNakerManchester/SpiNNFrontEndCommon to github.com/SpiNNakerManchester/SpiNNFrontEndCommon.git:gh-pages

--- c/search/functions_d.js
+++ c/search/functions_d.js
@@ -4,7 +4,7 @@ var searchData=
   ['oc_5fget_5fbest_5fmerge_1359',['oc_get_best_merge',['../ordered__covering_8h.html#a5071857be7a5d35aa0a3316d9232c8fd',1,'ordered_covering.h']]],
   ['oc_5fget_5finsertion_5fpoint_1360',['oc_get_insertion_point',['../ordered__covering_8h.html#a6df2d7d46162059a4d099e3828889ec8',1,'ordered_covering.h']]],
   ['oc_5fmerge_5fapply_1361',['oc_merge_apply',['../ordered__covering_8h.html#ac8f7b6172a620fc686eedfa606584801',1,'ordered_covering.h']]],
-  ['oc_5fminimise_1362',['oc_minimise',['../ordered__covering_8h.html#a7dc5a1924eea23a6134870e38ea9dbc6',1,'ordered_covering.h']]],
+  ['oc_5fminimise_1362',['oc_minimise',['../ordered__covering_8h.html#acbfc14b5cbfc6529554581ba2821b5cb',1,'ordered_covering.h']]],
   ['oc_5fup_5fcheck_1363',['oc_up_check',['../ordered__covering_8h.html#a398b9d3ca9c4919c0ca19022d7066d1b',1,'ordered_covering.h']]],
   ['order_5fbitfields_1364',['order_bitfields',['../bit__field__reader_8h.html#aca5e3310aacf901a560d7f137a4b65fd',1,'bit_field_reader.h']]]
 ];
