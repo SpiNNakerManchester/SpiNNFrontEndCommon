61c56cf4c69433b8d4e4d8f739bfd4f00a90b44f
alan-stokes <alan.barry.stokes@gmail.com>
2020-09-30 10:13:27 +0100

    bye bye app edge traffic type

--- spinn_front_end_common/interface/interface_functions/insert_edges_to_extra_monitor_functionality.py
+++ spinn_front_end_common/interface/interface_functions/insert_edges_to_extra_monitor_functionality.py
@@ -109,8 +109,7 @@ class InsertEdgesToExtraMonitorFunctionality(object):
                 application_graph, vertex.app_vertex, gatherer.app_vertex)
             if app_edge is None:
                 app_edge = ApplicationEdge(
-                    vertex.app_vertex, gatherer.app_vertex,
-                    traffic_type=DataSpeedUp.TRAFFIC_TYPE)
+                    vertex.app_vertex, gatherer.app_vertex)
                 application_graph.add_edge(
                     app_edge, PARTITION_ID_FOR_MULTICAST_DATA_SPEED_UP)
             # Use the application edge to build the machine edge
