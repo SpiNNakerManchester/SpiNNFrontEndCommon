3fe2f235be6f83d69716a0853c2422d16bd128c9
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2020-10-19 11:54:45 +0100

    New schema (tweak) and use of said schema
    No change in ingest code needed

--- spinn_front_end_common/interface/provenance/router_prov_mapper.py
+++ spinn_front_end_common/interface/provenance/router_prov_mapper.py
@@ -41,50 +41,62 @@ PLOTTABLES = (
     "Dumped_from_a_Link",
     "Dumped_from_a_processor",
     "Error")
+HAVE_INSERTION_ORDER = 1  # So we don't try schema errors several times
 
 
-def router_prov_details(db_filename, info):
+def _do_query(db, description):
+    # Does the query in one of two ways, depending on schema version
+    global HAVE_INSERTION_ORDER
+    if HAVE_INSERTION_ORDER:
+        try:
+            return db.execute("""
+                SELECT source_name AS "source", x, y, p,
+                    description_name AS "description",
+                    the_value AS "value"
+                FROM provenance_view
+                WHERE description LIKE ?
+                GROUP BY x, y, p
+                HAVING insertion_order = MAX(insertion_order)
+                """, (description, ))
+        except sqlite3.Error:
+            HAVE_INSERTION_ORDER = 0
+    return db.execute("""
+        SELECT source_name AS "source", x, y, p,
+            description_name AS "description",
+            MAX(the_value) AS "value"
+        FROM provenance_view
+        WHERE description LIKE ?
+        GROUP BY x, y, p
+        """, (description, ))
+
+
+def router_prov_details(db, info):
     data = []
     xs = []
     ys = []
     src = None
     name = None
-    with sqlite3.connect(db_filename) as db:
-        db.row_factory = sqlite3.Row
-        for row in db.execute("""
-                SELECT source_name AS "source", x, y, p,
-                    description_name AS "description",
-                    MAX(the_value) AS "value"
-                FROM provenance_view
-                WHERE description LIKE ?
-                GROUP BY x, y, p
-                """, ("%" + info + "%", )):
-            # Why is the provenance data duplicated sometimes?
-            # At least we know these counts monotonically increase
-            if src is None:
-                src = row["source"]
-            if name is None:
-                name = row["description"]
-            data.append((row["x"], row["y"], row["p"], row["value"]))
-            xs.append(row["x"])
-            ys.append(row["y"])
+    for row in _do_query(db, "%" + info + "%"):
+        if src is None:
+            src = row["source"]
+        if name is None:
+            name = row["description"]
+        data.append((row["x"], row["y"], row["p"], row["value"]))
+        xs.append(row["x"])
+        ys.append(row["y"])
     ary = numpy.full((max(ys) + 1, max(xs) + 1), float("NaN"))
     for (x, y, _p, value) in data:
         ary[y, x] = value
     return (src + "/" + name).replace("_", " "), max(xs) + 1, max(ys) + 1, ary
 
 
-def router_plot_data(db_filename, key, output_filename):
-    # Check the existence of the database here
-    # if the DB isn't there, the errors are otherwise *weird* if we don't check
-    if not os.path.exists(db_filename):
-        raise Exception("no such DB: " + db_filename)
+def router_plot_data(db, key, output_filename):
     # Import here because otherwise CI fails
     # pylint: disable=import-error
     import matplotlib.pyplot as plot
     import seaborn
     print("creating " + output_filename)
-    (title, width, height, data) = router_prov_details(db_filename, key)
+    (title, width, height, data) = router_prov_details(db, key)
     _fig, ax = plot.subplots(figsize=(width, height))
     plot.title(title)
     ax.set_xticks([])
@@ -102,8 +114,15 @@ def main():
     if len(sys.argv) != 2:
         raise Exception(
             "wrong number of arguments: needs just the prov DB filename")
-    for term in PLOTTABLES:
-        router_plot_data(sys.argv[1], term, term + ".png")
+    db_filename = sys.argv[1]
+    # Check the existence of the database here
+    # if the DB isn't there, the errors are otherwise *weird* if we don't check
+    if not os.path.exists(db_filename):
+        raise Exception("no such DB: " + db_filename)
+    with sqlite3.connect(db_filename) as db:
+        db.row_factory = sqlite3.Row
+        for term in PLOTTABLES:
+            router_plot_data(db, term, term + ".png")
 
 
 if __name__ == "__main__":
