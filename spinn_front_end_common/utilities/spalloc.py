b69fd008acf6f8ef15a77c0df9caba2ea25ad6b1
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2021-08-02 15:08:47 +0100

    Split out session handling, added some debug support

--- spinn_front_end_common/utilities/spalloc.py
+++ spinn_front_end_common/utilities/spalloc.py
@@ -12,96 +12,170 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-import enum
-import logging
-import multiprocessing
+from enum import IntEnum
+from functools import wraps
+from logging import getLogger
+from multiprocessing import Process, Queue
 from packaging.version import Version
-import queue
+from queue import Empty
 import re
 import requests
+from urllib.parse import urlparse, urlunparse
 from spinn_utilities.log import FormatAdapter
 
-logger = FormatAdapter(logging.getLogger(__name__))
-
+logger = FormatAdapter(getLogger(__name__))
 _S = "JSESSIONID"
+#: Enable detailed debugging by setting to True
+_debug_pretty_print = False
 
 
-class SpallocClient:
+def _clean_url(url):
+    """
+    Add a ``/`` to the end of the path part of a URL if there isn't one.
+
+    :param str url:
+    :rtype: str
+    """
+    r = urlparse(url)
+    parts = list(r)
+    # Add a / to the end of the path if it isn't there
+    if not parts[2].endswith("/"):
+        parts[2] += "/"
+    return urlunparse(parts)
+
+
+def _may_renew(method):
+    def pp_req(req):
+        """
+        :param ~requests.PreparedRequest req:
+        """
+        print('{}\n{}\r\n{}\r\n\r\n{}'.format(
+            '>>>>>>>>>>>START>>>>>>>>>>>',
+            req.method + ' ' + req.url,
+            '\r\n'.join('{}: {}'.format(*kv) for kv in req.headers.items()),
+            req.body if req.body else ""))
+
+    def pp_resp(resp):
+        """
+        :param ~requests.Response resp:
+        """
+        print('{}\n{}\r\n{}\r\n\r\n{}'.format(
+            '<<<<<<<<<<<START<<<<<<<<<<<',
+            str(resp.status_code) + " " + resp.reason,
+            '\r\n'.join('{}: {}'.format(*kv) for kv in resp.headers.items()),
+            # Assume we only get textual responses
+            str(resp.content, "UTF-8") if resp.content else ""))
+
+    @wraps(method)
+    def call(self, *args, **kwargs):
+        renew_count = 0
+        while True:
+            r = method(self, *args, **kwargs)
+            if _debug_pretty_print:
+                pp_req(r.request)
+                pp_resp(r)
+            if _S in r.cookies:
+                self._session_id = r.cookies[_S]
+            if r.status_code != 401 or not renew_count:
+                return r
+            self.renew()
+            renew_count += 1
+
+    return call
+
+
+class _Session:
+    __slots__ = (
+        "__login_form_url", "__login_submit_url", "__srv_base",
+        "__username", "__password",
+        "_session_id", "__csrf", "__csrf_header")
+
     def __init__(self, service_url, username, password):
         """
-        :param str service_url:
-        :param str username:
-        :param str password:
+        :param str service_url: The reference to the service.
+            *Should not* include a username or password in it.
+        :param str username: The user name to use
+        :param str password: The password to use
         """
-        # pylint: disable=abstract-class-instantiated
-        self.__url = service_url
-        # TODO better way of building these URLs
-        self.__login_form_url = self.__url + "system/login.html"
-        self.__login_submit_url = self.__url + "system/perform_login"
-        self.__srv_base = self.__url + "srv/spalloc/"
+        url = _clean_url(service_url)
+        self.__login_form_url = url + "system/login.html"
+        self.__login_submit_url = url + "system/perform_login"
+        self.__srv_base = url + "srv/spalloc/"
         self.__username = username
         self.__password = password
-        self._renew()
-        logger.info("established session to {} for {}", service_url, username)
 
-    def _get(self, url, **kwargs):
-        while True:
-            cookies = {_S: self.__session_id}
-            params = kwargs if kwargs else None
-            r = requests.get(url, params=params, cookies=cookies,
-                             allow_redirects=False)
-            logger.debug("GET {} returned {}", url, r.status_code)
-            if _S in r.cookies:
-                self.__session_id = r.cookies[_S]
-            if r.status_code != 401:
-                break
-            self._renew()
+    @_may_renew
+    def get(self, url, **kwargs):
+        """
+        Do an HTTP ``GET`` in the session.
+
+        :param str url:
+        :rtype: ~requests.Response
+        """
+        params = kwargs if kwargs else None
+        cookies = {_S: self._session_id}
+        r = requests.get(url, params=params, cookies=cookies,
+                         allow_redirects=False)
+        logger.debug("GET {} returned {}", url, r.status_code)
         return r
 
-    def _post(self, url, jsonobj):
-        while True:
-            cookies = {_S: self.__session_id}
-            headers = {self.__csrf_header: self.__csrf}
-            r = requests.post(url, json=jsonobj, cookies=cookies,
-                              headers=headers, allow_redirects=False)
-            logger.debug("POST {} returned {}", url, r.status_code)
-            if _S in r.cookies:
-                self.__session_id = r.cookies[_S]
-            if r.status_code != 401:
-                break
-            self._renew()
+    @_may_renew
+    def post(self, url, jsonobj, **kwargs):
+        """
+        Do an HTTP ``POST`` in the session.
+
+        :param str url:
+        :param dict jsonobj:
+        :rtype: ~requests.Response
+        """
+        params = kwargs if kwargs else None
+        cookies, headers = self._credentials
+        r = requests.post(url, params=params, json=jsonobj,
+                          cookies=cookies, headers=headers,
+                          allow_redirects=False)
+        logger.debug("POST {} returned {}", url, r.status_code)
         return r
 
-    def _put(self, url, data):
-        while True:
-            cookies = {_S: self.__session_id}
-            headers = {self.__csrf_header: self.__csrf}
-            r = requests.put(url, data=data, cookies=cookies,
-                             headers=headers, allow_redirects=False)
-            logger.debug("PUT {} returned {}", url, r.status_code)
-            if _S in r.cookies:
-                self.__session_id = r.cookies[_S]
-            if r.status_code != 401:
-                break
-            self._renew()
+    @_may_renew
+    def put(self, url, data, **kwargs):
+        """
+        Do an HTTP ``PUT`` in the session. Puts plain text *OR* JSON!
+
+        :param str url:
+        :param str data:
+        :rtype: ~requests.Response
+        """
+        params = kwargs if kwargs else None
+        cookies, headers = self._credentials
+        r = requests.put(url, params=params, data=data,
+                         cookies=cookies, headers=headers,
+                         allow_redirects=False)
+        logger.debug("PUT {} returned {}", url, r.status_code)
         return r
 
-    def _delete(self, url, **kwargs):
-        while True:
-            params = kwargs if kwargs else None
-            cookies = {_S: self.__session_id}
-            headers = {self.__csrf_header: self.__csrf}
-            r = requests.delete(url, params=params, cookies=cookies,
-                                headers=headers, allow_redirects=False)
-            logger.debug("DELETE {} returned {}", url, r.status_code)
-            if _S in r.cookies:
-                self.__session_id = r.cookies[_S]
-            if r.status_code != 401:
-                break
-            self._renew()
+    @_may_renew
+    def delete(self, url, **kwargs):
+        """
+        Do an HTTP ``DELETE`` in the session.
+
+        :param str url:
+        :rtype: ~requests.Response
+        """
+        params = kwargs if kwargs else None
+        cookies, headers = self._credentials
+        r = requests.delete(url, params=params, cookies=cookies,
+                            headers=headers, allow_redirects=False)
+        logger.debug("DELETE {} returned {}", url, r.status_code)
         return r
 
-    def _renew(self):
+    def renew(self):
+        """
+        Renews the session, logging the user into it so that state modification
+        operations can be performed.
+
+        :returns: Description of the root of the service, without CSRF data
+        :rtype: dict
+        """
         # Step one: a temporary session so we can log in
         csrf_matcher = re.compile(
             r"""<input type="hidden" name="_csrf" value="(.*)" />""")
@@ -127,30 +201,68 @@ class SpallocClient:
                           data=form)
         logger.debug("POST {} returned {}",
                      self.__login_submit_url, r.status_code)
-        self.__session_id = r.cookies[_S]
+        self._session_id = r.cookies[_S]
         # We don't need to follow that redirect
 
         # Step three: get the basic service data and new CSRF token
-        r = self._get(self.__srv_base)
-        logger.debug("GET {} returned {}", self.__srv_base, r.status_code)
-        obj = r.json()
+        obj = self.get(self.__srv_base).json()
+        self.__csrf_header = obj["csrf-header"]
+        self.__csrf = obj["csrf-token"]
+        del obj["csrf-header"]
+        del obj["csrf-token"]
+        return obj
+
+    @property
+    def _credentials(self):
+        """
+        The credentials for requests. *Serializable.*
+        """
+        cookies = {_S: self._session_id}
+        headers = {self.__csrf_header: self.__csrf}
+        return cookies, headers
+
+
+class SpallocClient:
+    """
+    Basic client library for talking to new Spalloc.
+    """
+    __slots__ = ("__session",
+                 "__machines_url", "__jobs_url", "version")
+
+    def __init__(self, service_url, username, password):
+        """
+        :param str service_url: The reference to the service.
+            *Should not* include a username or password in it.
+        :param str username: The user name to use
+        :param str password: The password to use
+        """
+        self.__session = _Session(service_url, username, password)
+        obj = self.__session.renew()
         v = obj["version"]
         self.version = Version(
             f"{v['major-version']}.{v['minor-version']}.{v['revision']}")
-        self.__csrf_header = obj["csrf-header"]
-        self.__csrf = obj["csrf-token"]
         self.__machines_url = obj["machines-ref"]
         self.__jobs_url = obj["jobs-ref"]
+        logger.info("established session to {} for {}", service_url, username)
 
     def list_machines(self):
         """
+        Get the machines supported by the server.
+
         :rtype: dict(str,SpallocMachine)
         """
-        obj = self._get(self.__machines_url).json()
+        obj = self.__session.get(self.__machines_url).json()
         return {m["name"]: SpallocMachine(self, m) for m in obj["machines"]}
 
     def create_job(self, num_boards, machine_name=None, keepalive=45):
         """
+        Create a job with a specified number of boards.
+
+        :param int num_boards: How many boards to ask for
+        :param str machine_name: Which machine to run on? If omitted, the
+            service's machine tagged with ``default`` will be used.
+        :param int keepalive: After how many seconds of no activity should a
+            job become eligible for automatic pruning?
         :rtype: SpallocJob
         """
         create = {
@@ -161,7 +273,7 @@ class SpallocClient:
             create["machine-name"] = machine_name
         else:
             create["tags"] = ["default"]
-        r = self._post(self.__jobs_url, create)
+        r = self.__session.post(self.__jobs_url, create)
         url = r.headers["Location"]
         return SpallocJob(self, url)
 
@@ -173,10 +285,12 @@ class SpallocClient:
         to do significant amounts of CPU-intensive work.
 
         :param SpallocJob job:
+            The job to keep alive
         :return:
             Some kind of closeable task handle; closing it terminates the task.
+            Destroying the job will also terminate the task.
         """
-        term_queue = multiprocessing.Queue(1)
+        term_queue = Queue(1)
 
         class Closer:
             def close(self):
@@ -184,9 +298,8 @@ class SpallocClient:
 
         closer = Closer()
         job._keepalive_handle = closer
-        cookies = {_S: self.__session_id}
-        headers = {self.__csrf_header: self.__csrf}
-        p = multiprocessing.Process(target=_SpallocKeepalive, args=(
+        cookies, headers = self.__session._credentials
+        p = Process(target=_SpallocKeepalive, args=(
             job._keepalive_url, cookies, headers, 30, term_queue), daemon=True)
         p.start()
         return closer
@@ -199,7 +312,7 @@ def _SpallocKeepalive(url, cookies, headers, interval, term_queue):
         try:
             term_queue.get(True, interval)
             break
-        except queue.Empty:
+        except Empty:
             continue
 
 
@@ -209,19 +322,28 @@ class SpallocMachine:
 
     Don't make this yourself. Use :py:class:`SpallocClient` instead.
     """
+    __slots__ = ("__session", "__url",
+                 "name", "tags", "width", "height",
+                 "dead_boards", "dead_links")
 
-    def __init__(self, client, machine_data):
+    def __init__(self, session, machine_data):
         """
-        :param SpallocClient client:
+        :param _Session session:
         :param dict machine_data:
         """
-        self.__client = client
+        self.__session = session
+        #: The name of the machine.
         self.name = machine_data["name"]
+        #: The tags of the machine.
         self.tags = frozenset(machine_data["tags"])
-        self.__url = machine_data["uri"]
+        self.__url = _clean_url(machine_data["uri"])
+        #: The width of the machine, in boards.
         self.width = machine_data["width"]
+        #: The height of the machine, in boards.
         self.height = machine_data["height"]
+        #: The dead or out-of-service boards of the machine.
         self.dead_boards = machine_data["dead-boards"]
+        #: The dead or out-of-service links of the machine.
         self.dead_links = machine_data["dead-links"]
 
     @property
@@ -246,32 +368,39 @@ class SpallocJob:
 
     Don't make this yourself. Use :py:class:`SpallocClient` instead.
     """
+    __slots__ = ("__session", "__url", "__machine_url",
+                 "_keepalive_url", "__keepalive_handle")
 
-    def __init__(self, client, job_handle):
+    def __init__(self, session, job_handle):
         """
-        :param SpallocClient client:
-        :param job_handle:
+        :param _Session session:
+        :param str job_handle:
         """
         logger.info("established job at {}", job_handle)
-        self.__client = client
-        self.__url = job_handle
-        self.__machine_url = job_handle + "/machine"
-        self._keepalive_url = job_handle + "/keepalive"
+        self.__session = session
+        self.__url = _clean_url(job_handle)
+        self.__machine_url = self.__url + "machine"
+        self._keepalive_url = self.__url + "keepalive"
         self.__keepalive_handle = None
 
     def get_state(self):
         """
+        Get the current state of the machine.
+
         :rtype: SpallocState
         """
-        obj = self.__client._get(self.__url).json()
+        obj = self.__session.get(self.__url).json()
         # TODO check if states are reported by name!
         return SpallocState(obj["state"])
 
     def get_root_host(self):
         """
-        :rtype: str
+        Get the IP address for talking to the machine.
+
+        :return: The IP address, or ``None`` if not allocated.
+        :rtype: str or None
         """
-        r = self.__client._get(self.__machine_url)
+        r = self.__session.get(self.__machine_url)
         if r.status_code == 204:
             return None
         obj = r.json()
@@ -283,27 +412,37 @@ class SpallocJob:
 
     def wait_for_state_change(self, old_state):
         """
+        Wait until the allocation is not in the given old state.
+
         :param SpallocState old_state:
+            The state that we are looking to change out of.
+        :return: The state that the allocation is now in. Note that if the
+            machine gets destroyed, this will not wait for it.
         :rtype: SpallocState
         """
-        while True:
-            obj = self.__client._get(self.__url, wait="true").json()
+        while old_state != SpallocState.DESTROYED:
+            obj = self.__session.get(self.__url, wait="true").json()
             s = SpallocState(obj["state"])
             if s != old_state or s == SpallocState.DESTROYED:
                 return s
 
     def destroy(self, reason="finished"):
         """
-        :param str reason:
+        Destroy the job.
+
+        :param str reason: Why the job is being destroyed.
         """
         if self.__keepalive_handle:
             self.__keepalive_handle.close()
             self.__keepalive_handle = None
-        self.__client._delete(self.__url, reason=reason)
+        self.__session.delete(self.__url, reason=reason)
         logger.info("deleted job at {}", self.__url)
 
     def keepalive(self):
-        self.__client._put(self._keepalive_url, "alive")
+        """
+        Signal the job that we want it to stay alive for a while longer.
+        """
+        self.__session.put(self._keepalive_url, "alive")
 
     @property
     def _keepalive_handle(self):
@@ -316,9 +455,14 @@ class SpallocJob:
         self.__keepalive_handle = handle
 
 
-class SpallocState(enum.IntEnum):
+class SpallocState(IntEnum):
+    #: The job is in an unknown state.
     UNKNOWN = 0
+    #: The job is queued waiting for allocation.
     QUEUED = 1
+    #: The job is queued waiting for boards to power on or off.
     POWER = 2
+    #: The job is ready for user code to run on it.
     READY = 3
+    #: The job has been destroyed.
     DESTROYED = 4
