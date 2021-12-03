8f4eab83fdc8026e29e9b63bff33f1f97e372d7e
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2021-12-03 16:42:55 +0000

    Remove unused

--- spinn_front_end_common/interface/interface_functions/insert_extra_monitor_vertices_to_graphs.py
+++ spinn_front_end_common/interface/interface_functions/insert_extra_monitor_vertices_to_graphs.py
@@ -14,7 +14,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from spinn_utilities.progress_bar import ProgressBar
-from pacman.model.constraints.placer_constraints import ChipAndCoreConstraint
 from pacman.model.placements import Placement
 from spinn_front_end_common.utility_models import (
     DataSpeedUpPacketGatherMachineVertex, ExtraMonitorSupportMachineVertex)
