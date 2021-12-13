8dc42860c8022f29c519648ab0dc931ba4a6653b
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2021-12-13 12:03:08 +0000

    cap skippy

--- setup.py
+++ setup.py
@@ -83,7 +83,8 @@ setup(
                       "numpy > 1.13, < 1.21; python_version == '3.7'",
                       "numpy; python_version >= '3.8'",
                       "scipy >= 0.16.0, < 1.6; python_version == '3.6'",
-                      "scipy >= 0.16.0; python_version >= '3.7'"],
+                      "scipy >= 0.16.0, < 1.8; python_version == '3.7'",
+                      "scipy >= 0.16.0; python_version >= '3.8'"],
     extras_require={
         'plotting': [
             'matplotlib',
