35edef759a1c8488113f595a09102456bcf441c8
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2022-03-21 14:49:53 +0000

    Better drift report

--- spinn_front_end_common/utilities/report_functions/drift_report.py
+++ spinn_front_end_common/utilities/report_functions/drift_report.py
@@ -30,11 +30,10 @@ def drift_report(txrx):
     ethernet_only = get_config_bool(
             "Reports", "drift_report_ethernet_only")
     machine = txrx.get_machine_details()
-    chips = machine.chips
+    eth_chips = machine.ethernet_connected_chips
     n_chips = machine.n_chips
     if ethernet_only:
-        chips = machine.ethernet_connected_chips
-        n_chips = len(chips)
+        n_chips = len(eth_chips)
 
     # create file path
     directory_name = os.path.join(
@@ -43,8 +42,13 @@ def drift_report(txrx):
     # If the file is new, write a header
     if not os.path.exists(directory_name):
         with open(directory_name, "w") as writer:
-            for chip in chips:
-                writer.write(f'"{chip.x}-{chip.y}",')
+            for eth_chip in eth_chips:
+                if ethernet_only:
+                    writer.write(f'"{eth_chip.x} {eth_chip.y}",')
+                else:
+                    for chip in machine.get_chips_by_ethernet(
+                            eth_chip.x, eth_chip.y):
+                        writer.write(f'"{chip.x} {chip.y}"')
             writer.write("\n")
 
     # create the progress bar for end users
@@ -52,10 +56,21 @@ def drift_report(txrx):
 
     # iterate over ethernet chips and then the chips on that board
     with open(directory_name, "a") as writer:
-        for chip in progress.over(chips):
-            drift = txrx._get_sv_data(
-                chip.x, chip.y, SystemVariableDefinition.clock_drift)
-            drift = struct.unpack("<i", struct.pack("<I", drift))[0]
-            drift = drift / (1 << 17)
-            writer.write(f'"{drift}",')
+        for eth_chip in eth_chips:
+            if ethernet_only:
+                __write_drift(txrx, eth_chip, writer)
+                progress.update()
+            else:
+                for chip in machine.get_chips_by_ethernet(
+                        eth_chip.x, eth_chip.y):
+                    __write_drift(txrx, chip, writer)
+                    progress.update()
         writer.write("\n")
+
+
+def __write_drift(txrx, chip, writer):
+    drift = txrx._get_sv_data(
+        chip.x, chip.y, SystemVariableDefinition.clock_drift)
+    drift = struct.unpack("<i", struct.pack("<I", drift))[0]
+    drift = drift / (1 << 17)
+    writer.write(f'"{drift}",')
