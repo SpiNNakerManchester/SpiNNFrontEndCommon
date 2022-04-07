e85176188e0fc1d7c0b4c9633cefeb37fe09bc1e
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2022-04-07 10:31:43 +0100

    Make exception less verbose

--- spinn_front_end_common/utilities/emergency_recovery.py
+++ spinn_front_end_common/utilities/emergency_recovery.py
@@ -55,9 +55,14 @@ def _emergency_state_check(txrx, app_id):
                         infos.add_processor(chip.x, chip.y, p, info)
                 except Exception:
                     errors.append((chip.x, chip.y, p))
-        logger.warning(txrx.get_core_status_string(infos))
-        logger.warning("Could not read information from cores {}".format(
-            errors))
+        if len(infos):
+            logger.warning(txrx.get_core_status_string(infos))
+        if len(len(errors) > 10):
+            logger.warning("Could not read information from {} cores".format(
+                len(errors)))
+        else:
+            logger.warning("Could not read information from cores {}".format(
+                errors))
 
 
 # TRICKY POINT: Have to delay the import to here because of import circularity
