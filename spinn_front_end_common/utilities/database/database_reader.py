82f00893f26eb67ff8606c88e47f73521046ae3a
Donal K. Fellows <donal.k.fellows@manchester.ac.uk>
2023-05-12 16:59:47 +0100

    Oops

--- spinn_front_end_common/utilities/database/database_reader.py
+++ spinn_front_end_common/utilities/database/database_reader.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from spinn_front_end_common.utilities.sqlite_db import SQLiteDB, Isolation
+from spinn_front_end_common.utilities.sqlite_db import SQLiteDB
 from spinnman.spalloc import SpallocClient
 
 
