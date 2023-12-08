373c0ff0246802b9a999d6cb312b3597248b6321
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-12-08 14:26:36 +0000

    overrides typing

--- spinn_front_end_common/utility_models/reverse_ip_tag_multicast_source_machine_vertex.py
+++ spinn_front_end_common/utility_models/reverse_ip_tag_multicast_source_machine_vertex.py
@@ -746,7 +746,7 @@ class ReverseIPTagMulticastSourceMachineVertex(
         return [0]
 
     @overrides(AbstractReceiveBuffersToHost.get_recording_region_base_address)
-    def get_recording_region_base_address(self, placement) -> int:
+    def get_recording_region_base_address(self, placement: Placement) -> int:
         return locate_memory_region_for_placement(
             placement, self._REGIONS.RECORDING)
 
