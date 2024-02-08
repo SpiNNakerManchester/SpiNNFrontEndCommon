e4626ce1e81406460126e21942ded5429f6c1f12
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2024-02-08 06:19:35 +0000

    remove unused import

--- spinn_front_end_common/interface/provenance/provides_provenance_data_from_machine_impl.py
+++ spinn_front_end_common/interface/provenance/provides_provenance_data_from_machine_impl.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from enum import Enum
 from typing import Sequence, Tuple
 from spinn_utilities.abstract_base import abstractmethod
 from spinn_utilities.overrides import overrides
