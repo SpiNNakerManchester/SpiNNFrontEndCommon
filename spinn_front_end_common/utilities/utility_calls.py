b03848c68bb6cf4a5d8daa4a00a57b0f779cf0fb
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2024-03-04 17:28:36 +0000

    use correct chip method

--- spinn_front_end_common/utilities/utility_calls.py
+++ spinn_front_end_common/utilities/utility_calls.py
@@ -209,5 +209,5 @@ def pick_core_for_system_placement(
     :return: a core that a system placement could be put on
     :rtype: int
     """
-    cores = [chip.user_processors]
+    cores = list(chip.user_processors_ids)
     return cores[system_placements.n_placements_on_chip(chip.x, chip.y)]
