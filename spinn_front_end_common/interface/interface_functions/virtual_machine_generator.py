f17ffb9ad1327f706a7f2c20345fff7cd786d654
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2024-04-12 07:53:30 +0100

    use n_boards_required for size of virtual board

--- spinn_front_end_common/interface/interface_functions/virtual_machine_generator.py
+++ spinn_front_end_common/interface/interface_functions/virtual_machine_generator.py
@@ -14,12 +14,14 @@
 
 import logging
 from spinn_utilities.config_holder import (
-    get_config_int, get_config_str_or_none, is_config_none)
+    get_config_int, get_config_int_or_none, get_config_str_or_none,
+    is_config_none)
 from spinn_utilities.log import FormatAdapter
 from spinn_machine import json_machine, Machine
 from spinn_machine.virtual_machine import (
-    virtual_machine, virtual_machine_by_chips)
+    virtual_machine, virtual_machine_by_boards, virtual_machine_by_chips)
 from spinn_front_end_common.data import FecDataView
+from spinn_front_end_common.utilities.exceptions import ConfigurationException
 logger = FormatAdapter(logging.getLogger(__name__))
 
 
@@ -34,14 +36,26 @@ def virtual_machine_generator() -> Machine:
 
     json_path = get_config_str_or_none("Machine", "json_path")
     if json_path is None:
-        if is_config_none("Machine", "width") and \
+        if is_config_none("Machine", "width") or \
                 is_config_none("Machine", "height"):
-            n_chips = FecDataView.get_n_chips_needed()
-            machine = virtual_machine_by_chips((n_chips))
+            if FecDataView.has_n_boards_required():
+                n_boards = FecDataView.get_n_boards_required()
+                machine = virtual_machine_by_boards((n_boards))
+            elif FecDataView.has_n_chips_needed():
+                n_chips = FecDataView.get_n_chips_needed()
+                machine = virtual_machine_by_chips((n_chips))
+            else:
+                height = get_config_int_or_none("Machine", "height")
+                width = get_config_int_or_none("Machine", "width")
+                raise ConfigurationException(
+                    "Unable to create a VirtualMachine at this time unless "
+                    "both width and heigth are specified in the cfg found "
+                    f"found {width=} {height=}")
         else:
             height = get_config_int("Machine", "height")
             width = get_config_int("Machine", "width")
-            machine = virtual_machine(width=width, height=height, validate=True)
+            machine = virtual_machine(
+                width=width, height=height, validate=True)
     else:
         if (not is_config_none("Machine", "width") or
                 not is_config_none("Machine", "height") or
