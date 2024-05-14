1ad779dad74ea017004f7d3922fa458962f3618f
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2024-05-14 11:08:59 +0100

    flake8

--- spinn_front_end_common/interface/interface_functions/spalloc_allocator.py
+++ spinn_front_end_common/interface/interface_functions/spalloc_allocator.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from contextlib import AbstractContextManager, ExitStack
+from contextlib import ExitStack
 import logging
 import math
 from typing import ContextManager, Dict, Tuple, Optional, Union, cast
