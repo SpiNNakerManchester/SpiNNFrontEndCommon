49aa4525f59589c9c9ed291c59ff740bcbe66934
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2024-05-24 07:44:40 +0100

    external_binaries is a cfg setting

--- spinn_front_end_common/interface/abstract_spinnaker_base.py
+++ spinn_front_end_common/interface/abstract_spinnaker_base.py
@@ -213,15 +213,10 @@ class AbstractSpinnakerBase(ConfigHandler):
         self._data_writer.register_binary_search_path(
             os.path.dirname(common_model_binaries.__file__))
 
-        if "SPINNAKER_EXTERNAL_BINARIES" in os.environ:
-             #self._data_writer.register_binary_search_path(os.path.join(
-             #   os.path.expanduser("~"), os.environ["EXTERNAL_BINARIES"]))
-             #self._data_writer.register_binary_search_path(
-             #    os.path.abspath(os.environ["EXTERNAL_BINARIES"]))
-             self._data_writer.register_binary_search_path(
-                 os.environ["SPINNAKER_EXTERNAL_BINARIES"])
-        else:
-            pop = 1/0
+        external_binaries = get_config_str_or_none(
+            "Mapping", "external_binaries")
+        if external_binaries is not None:
+             self._data_writer.register_binary_search_path(external_binaries)
 
         self._data_writer.set_machine_generator(self._get_machine)
         FecTimer.end_category(TimerCategory.SETTING_UP)
