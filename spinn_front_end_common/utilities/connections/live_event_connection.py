90526ada35ed6e30472118a3143cbe94851f2f0f
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2024-07-05 11:59:39 +0100

    Control the sending of packets more to avoid losses

--- spinn_front_end_common/utilities/connections/live_event_connection.py
+++ spinn_front_end_common/utilities/connections/live_event_connection.py
@@ -57,6 +57,9 @@ _MAX_HALF_KEYS_PER_PACKET = 127
 # The maximum number of 32-bit keys with payloads that will fit in a packet
 _MAX_FULL_KEYS_PAYLOADS_PER_PACKET = 31
 
+# The maximum number of packets to send before pausing
+_MAX_SEND_BEFORE_PAUSE = 6
+
 # Decoding of a single short value
 _ONE_SHORT = struct.Struct("<H")
 
@@ -665,6 +668,7 @@ class LiveEventConnection(DatabaseConnection):
             msg_type = EIEIOType.KEY_32_BIT
 
         pos = 0
+        packets_sent = 0
         x, y, p, ip_address = self.__send_address_details[label]
         while pos < len(atom_ids):
             message = EIEIODataMessage.create(msg_type)
@@ -678,6 +682,10 @@ class LiveEventConnection(DatabaseConnection):
                 events_in_packet += 1
 
             self._send(message, x, y, p, ip_address)
+            packets_sent += 1
+            if (packets_sent % _MAX_SEND_BEFORE_PAUSE == 0 and
+                    pos < len(atom_ids)):
+                sleep(0.1)
 
     def send_event_with_payload(
             self, label: str, atom_id: int, payload: int):
@@ -705,6 +713,7 @@ class LiveEventConnection(DatabaseConnection):
         msg_type = EIEIOType.KEY_PAYLOAD_32_BIT
         max_keys = _MAX_FULL_KEYS_PAYLOADS_PER_PACKET
         pos = 0
+        packets_sent = 0
         x, y, p, ip_address = self.__send_address_details[label]
         while pos < len(atom_ids_and_payloads):
             message = EIEIODataMessage.create(msg_type)
@@ -717,6 +726,10 @@ class LiveEventConnection(DatabaseConnection):
                 events += 1
 
             self._send(message, x, y, p, ip_address)
+            packets_sent += 1
+            if (packets_sent % _MAX_SEND_BEFORE_PAUSE == 0 and
+                    pos < len(atom_ids_and_payloads)):
+                sleep(0.1)
 
     def send_eieio_message(
             self, message: AbstractEIEIOMessage, label: str):
