7c30f7f77b8e6b6005edbd1bb1a3c50527acf704
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2024-08-23 09:23:19 +0100

    merge tests

--- unittests/interface/buffer_management/test_buffered_manager.py
+++ unittests/interface/buffer_management/test_buffered_manager.py
@@ -14,16 +14,35 @@
 
 import unittest
 import os
+from typing import Sequence
+
 from spinn_utilities.config_holder import set_config
+from spinn_utilities.overrides import overrides
+
 from spinn_machine.version.version_strings import VersionStrings
+
 from pacman.model.graphs.machine import SimpleMachineVertex
 from pacman.model.placements import Placement, Placements
+
 from spinn_front_end_common.data.fec_data_writer import FecDataWriter
 from spinn_front_end_common.interface.buffer_management import BufferManager
+from spinn_front_end_common.interface.buffer_management.buffer_models import (
+    AbstractReceiveBuffersToHost)
 from spinn_front_end_common.interface.buffer_management.storage_objects \
     import BufferDatabase
 from spinn_front_end_common.interface.config_setup import unittest_setup
+from spinn_front_end_common.utilities.exceptions import (
+    BufferedRegionNotPresent)
 
+class MockAbstractReceiveBuffersToHost(SimpleMachineVertex,
+                                       AbstractReceiveBuffersToHost):
+    @overrides(AbstractReceiveBuffersToHost.get_recorded_region_ids)
+    def get_recorded_region_ids(self) -> Sequence[int]:
+        return[0]
+
+    @overrides(AbstractReceiveBuffersToHost.get_recording_region_base_address)
+    def get_recording_region_base_address(self, placement: Placement) -> int:
+        raise NotImplementedError
 
 class TestBufferedDatabase(unittest.TestCase):
 
@@ -32,66 +51,74 @@ class TestBufferedDatabase(unittest.TestCase):
 
     def test_use_database(self):
         set_config("Machine", "versions", VersionStrings.ANY.text)
+        writer = FecDataWriter.mock()
         f = BufferDatabase.default_database_file()
         self.assertFalse(os.path.isfile(f), "no existing DB at first")
 
-        with BufferDatabase() as brd:
-            self.assertTrue(os.path.isfile(f), "DB now exists")
-
-            with self.assertRaises(LookupError):
-                data, missing = brd.get_region_data(0, 0, 0, 0)
-
-            brd.start_new_extraction()
-            brd.store_data_in_region_buffer(0, 0, 0, 0, False, b"abc")
-            data, missing = brd.get_region_data(0, 0, 0, 0)
-            self.assertFalse(missing, "data shouldn't be 'missing'")
-            self.assertEqual(bytes(data), b"abc")
-
-            brd.start_new_extraction()
-            brd.store_data_in_region_buffer(0, 0, 0, 0, False, b"def")
-            data, missing = brd.get_region_data(0, 0, 0, 0)
-            self.assertFalse(missing, "data shouldn't be 'missing'")
-            self.assertEqual(bytes(data), b"abcdef")
-
-            brd.start_new_extraction()
-            brd.store_data_in_region_buffer(0, 0, 0, 0, True, b"g")
-            data, missing = brd.get_region_data(0, 0, 0, 0)
-            self.assertTrue(missing, "data should be 'missing'")
-            self.assertEqual(bytes(data), b"abcdefg")
-
-            data, missing = brd.get_region_data_by_extraction_id(0, 0, 0, 0, 2)
-            self.assertFalse(missing, "data shouldn't be 'missing'")
-            self.assertEqual(bytes(data), b"def")
-
-            data, missing = brd.get_region_data_by_extraction_id(0, 0, 0, 0, -1)
-            self.assertTrue(missing, "data should be 'missing'")
-            self.assertEqual(bytes(data), b"g")
-
-            self.assertTrue(os.path.isfile(f), "DB still exists")
-
-    def test_placements(self):
-        set_config("Machine", "versions", VersionStrings.BIG.text)
-        writer = FecDataWriter.mock()
         info = Placements([])
-        p1 = Placement(SimpleMachineVertex(None, label="V1"), 1, 2, 3)
+        p1 = Placement(
+            MockAbstractReceiveBuffersToHost(None, label="V1"), 1, 2, 3)
         info.add_placement(p1)
         v2 = SimpleMachineVertex(None, label="V2")
         p2 = Placement(v2, 1, 2, 5)
         info.add_placement(p2)
         info.add_placement(Placement(SimpleMachineVertex(None), 2, 2, 3))
         writer.set_placements(info)
-        with BufferDatabase() as db:
-            db.start_new_extraction()
-            db.store_data_in_region_buffer(1, 2, 3, 0, False, b"abc")
-            db.store_vertex_labels()
-            label = db.get_core_name(1, 2, 3)
+        bm = BufferManager()
+        with BufferDatabase() as brd:
+            self.assertTrue(os.path.isfile(f), "DB now exists")
+
+            brd.store_vertex_labels()
+            label = brd.get_core_name(1, 2, 3)
             self.assertEqual("V1", label)
-            label = db.get_core_name(1, 2, 5)
+            label = brd.get_core_name(1, 2, 5)
             self.assertEqual("V2", label)
-            label = db.get_core_name(4, 3, 0)
+            label = brd.get_core_name(4, 3, 0)
             self.assertEqual("SCAMP(OS)_4:3", label)
 
-        bm = BufferManager()
+            with self.assertRaises(LookupError):
+                brd.get_region_data(1, 2, 3, 0)
+                
+        with self.assertRaises(BufferedRegionNotPresent):
+            bm.get_data_by_placement(p1, 0)
+
+        with BufferDatabase() as brd:
+            brd.start_new_extraction()
+            brd.store_data_in_region_buffer(1, 2, 3, 0, False, b"abc")
+            data, missing = brd.get_region_data(1, 2, 3, 0)
+            self.assertFalse(missing, "data shouldn't be 'missing'")
+            self.assertEqual(bytes(data), b"abc")
+
         data, missing = bm.get_data_by_placement(p1, 0)
-        self.assertFalse(missing, "data should not be 'missing'")
+        self.assertFalse(missing, "data shouldn't be 'missing'")
         self.assertEqual(bytes(data), b"abc")
+
+        with BufferDatabase() as brd:
+            brd.start_new_extraction()
+            brd.store_data_in_region_buffer(1, 2, 3, 0, False, b"def")
+            data, missing = brd.get_region_data(1, 2, 3, 0)
+            self.assertFalse(missing, "data shouldn't be 'missing'")
+            self.assertEqual(bytes(data), b"abcdef")
+
+            brd.start_new_extraction()
+            brd.store_data_in_region_buffer(1, 2, 3, 0, True, b"g")
+            data, missing = brd.get_region_data(1, 2, 3, 0)
+            self.assertTrue(missing, "data should be 'missing'")
+            self.assertEqual(bytes(data), b"abcdefg")
+
+        data, missing = bm.get_data_by_placement(p1, 0)
+        self.assertTrue(missing, "data should be 'missing'")
+        self.assertEqual(bytes(data), b"abcdefg")
+
+        with BufferDatabase() as brd:
+            data, missing = brd.get_region_data_by_extraction_id(1, 2, 3, 0, 2)
+            self.assertFalse(missing, "data shouldn't be 'missing'")
+            self.assertEqual(bytes(data), b"def")
+
+            data, missing = brd.get_region_data_by_extraction_id(1, 2, 3, 0, -1)
+            self.assertTrue(missing, "data should be 'missing'")
+            self.assertEqual(bytes(data), b"g")
+
+            self.assertTrue(os.path.isfile(f), "DB still exists")
+
+
