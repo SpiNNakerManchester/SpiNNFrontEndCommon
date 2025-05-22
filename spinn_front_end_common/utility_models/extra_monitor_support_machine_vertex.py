5f2a2570862d2285dbbb0c7e81945a0f72faf6a6
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-05-22 09:38:07 +0100

    remove pylint: disable=unsubscriptable-object

--- spinn_front_end_common/utility_models/extra_monitor_support_machine_vertex.py
+++ spinn_front_end_common/utility_models/extra_monitor_support_machine_vertex.py
@@ -317,7 +317,6 @@ class ExtraMonitorSupportMachineVertex(
         router_timeout_keys = \
             FecDataView.get_system_multicast_router_timeout_keys()
         # Write the base key for multicast communication
-        # pylint: disable=unsubscriptable-object
         spec.write_value(router_timeout_keys[
             chip.nearest_ethernet_x, chip.nearest_ethernet_y])
 
@@ -337,7 +336,6 @@ class ExtraMonitorSupportMachineVertex(
         # write address key and data key
         mc_data_chips_to_keys = \
             FecDataView.get_data_in_multicast_key_to_chip_map()
-        # pylint: disable=unsubscriptable-object
         base_key = mc_data_chips_to_keys[chip.x, chip.y]
         spec.write_value(base_key + _KeyOffsets.ADDRESS_KEY_OFFSET)
         spec.write_value(base_key + _KeyOffsets.DATA_KEY_OFFSET)
