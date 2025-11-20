43abb69d04323ee1b9317a80e227d3d9a847289b
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-11-20 09:08:30 +0000

    char database id

--- c_common/models/fec_models.mk
+++ c_common/models/fec_models.mk
@@ -16,7 +16,7 @@ MAKEFILE_PATH := $(abspath $(lastword $(MAKEFILE_LIST)))
 CURRENT_DIR := $(dir $(MAKEFILE_PATH))
 APP_OUTPUT_DIR := $(abspath $(CURRENT_DIR)../../spinn_front_end_common/common_model_binaries/)/
 
-DATABASE_ID = 1
+DATABASE_ID = F
 include $(CURRENT_DIR)../front_end_common_lib/local.mk
 
 .PRECIOUS: $(MODIFIED_DIR)%.c
