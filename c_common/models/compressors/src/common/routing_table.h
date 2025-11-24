9e829a0e3718762be6d32a33bcc05bd80d8a6002
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-11-24 15:00:58 +0000

    remove rt_single

--- c_common/models/compressors/src/common/routing_table.h
+++ c_common/models/compressors/src/common/routing_table.h
@@ -143,4 +143,101 @@ static inline key_mask_t key_mask_merge(key_mask_t a, key_mask_t b) {
     return c;
 }
 
+//! \brief flag for if a rtr_mc_set() failure.
+#define RTR_MC_SET_FAILED 0
+
+//! \brief The table being manipulated.
+//!
+//! This is common across all the functions in this file.
+table_t *table;
+
+//! \brief The header of the routing table information in the input data block.
+//!
+//! This is found looking for a memory block with the right tag.
+typedef struct {
+    //! Application ID to use to load the routing table. This can be left as `0`
+    //! to load routing entries with the same application ID that was used to
+    //! load this application.
+    uint32_t app_id;
+
+    //! flag that uses the available entries of the router table instead of
+    //! compressing as much as possible.
+    uint32_t compress_as_much_as_possible;
+
+    //! Initial size of the routing table.
+    uint32_t table_size;
+
+    //! Routing table entries
+    entry_t entries[];
+} header_t;
+
+int routing_table_get_n_entries(void) {
+    return table->size;
+}
+
+void routing_table_remove_from_size(int size_to_remove) {
+    table->size -= size_to_remove;
+}
+
+entry_t* routing_table_get_entry(uint32_t entry_id_to_find) {
+    return &table->entries[entry_id_to_find];
+}
+
+//! \brief Print the header object for debug purposes
+//! \param[in] header: the header to print
+void print_header(header_t *header) {
+    log_debug("app_id = %d", header->app_id);
+    log_debug("compress_as_much_as_possible = %d",
+            header->compress_as_much_as_possible);
+    log_debug("table_size = %d", header->table_size);
+}
+
+//! \brief Read a new copy of the routing table from SDRAM.
+//! \param[in] header: the header object
+static void read_table(header_t *header) {
+    table = MALLOC(
+        sizeof(uint32_t) + (sizeof(entry_t) * header->table_size));
+    if (table == NULL) {
+        log_error("failed to allocate memory for routing tables");
+        malloc_extras_terminate(EXIT_FAIL);
+    }
+    // Copy the size of the table
+    table->size = header->table_size;
+
+    // Copy in the routing table entries
+    spin1_memcpy(table->entries, header->entries,
+            sizeof(entry_t) * table->size);
+}
+
+//! \brief Load a routing table to the router.
+//! \param[in] app_id:
+//!     the app id for the routing table entries to be loaded under
+//! \return whether the table was loaded into the router
+bool load_routing_table(uint32_t app_id) {
+    // Try to allocate sufficient room for the routing table.
+    uint32_t entry_id = rtr_alloc_id(table->size, app_id);
+    if (entry_id == 0) {
+        log_error("Unable to allocate routing table of size %u\n", table->size);
+        return FALSE;
+    }
+
+    // Load entries into the table (provided the allocation succeeded).
+    // Note that although the allocation included the specified
+    // application ID we also need to include it as the most significant
+    // byte in the route (see `sark_hw.c`).
+    for (uint32_t i = 0; i < table->size; i++) {
+        entry_t entry = table->entries[i];
+        uint32_t route = entry.route | (app_id << 24);
+        uint success = rtr_mc_set(
+            entry_id + i, entry.key_mask.key, entry.key_mask.mask, route);
+        if (success == RTR_MC_SET_FAILED) {
+            log_warning("failed to set a router table entry at index %d",
+                    entry_id + i);
+        }
+    }
+
+    // Indicate we were able to allocate routing table entries.
+    return TRUE;
+}
+
 #endif  // __ROUTING_TABLE_H__
