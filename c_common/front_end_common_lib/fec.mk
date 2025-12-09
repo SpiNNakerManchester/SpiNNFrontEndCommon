3c43b525d8fadbdf3efe991f52b5fab8afafa7b8
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-12-09 08:36:37 +0000

    clean modified sources

--- c_common/front_end_common_lib/fec.mk
+++ c_common/front_end_common_lib/fec.mk
@@ -147,3 +147,4 @@ include $(SPINN_COMMON_INSTALL_DIR)/make/spinn_common.mk
 # Tidy and cleaning dependencies
 clean:
 	$(RM) $(TEMP_FILES) $(OBJECTS) $(BUILD_DIR)$(APP).elf $(BUILD_DIR)$(APP).txt $(ALL_TARGETS) $(BUILD_DIR)$(APP).nm $(BUILD_DIR)$(APP).elf $(BUILD_DIR)$(APP).bin
+	$(RM) -r $(ALL_MODIFIES_DIRS)
