dedf6da6f0f3c3d4957a1a37655326b86b8fd1dd
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2026-03-25 15:09:27 +0000

    update tests

--- fec_integration_tests/interface/interface_functions/test_database_interface.py
+++ fec_integration_tests/interface/interface_functions/test_database_interface.py
@@ -27,7 +27,7 @@ from pacman.model.resources import ConstantSDRAM
 from pacman.model.placements import Placements, Placement
 from pacman.model.routing_info import (
     RoutingInfo,
-    SpecificAppVertexRoutingInfo, SpecificMachineVertexRoutingInfo)
+    GlobalAppVertexRoutingInfo, GlobalMachineVertexRoutingInfo, BaseKeyAndMask)
 from pacman.model.tags.tags import Tags
 from pacman.model.partitioner_splitters import AbstractSplitterCommon
 from pacman.model.graphs.common.slice import Slice
@@ -106,13 +106,14 @@ def _add_rinfo(
         app_vertex: ApplicationVertex, partition_id: str,
         routing_info: RoutingInfo, base_key: int, app_mask: int, mac_mask: int,
         m_vertex_shift: int) -> None:
-    routing_info.add_routing_info(SpecificAppVertexRoutingInfo(
-        base_key, partition_id, app_vertex,
-        mac_mask, 1))
+    bkma = BaseKeyAndMask(base_key, app_mask)
+    routing_info.add_routing_info(GlobalAppVertexRoutingInfo(
+        bkma, partition_id, app_vertex, len(app_vertex.machine_vertices), mac_mask))
     for i, m_vertex in enumerate(app_vertex.machine_vertices):
-        routing_info.add_routing_info(SpecificMachineVertexRoutingInfo(
-            base_key | i << m_vertex_shift, app_mask | mac_mask,
-            partition_id, m_vertex, i))
+        m_key = base_key | i << m_vertex_shift
+        bkmm = BaseKeyAndMask(m_key, app_mask | mac_mask)
+        routing_info.add_routing_info(GlobalMachineVertexRoutingInfo(
+            bkmm, partition_id, m_vertex, i, app_mask))
 
 
 def _place_vertices(app_vertexes: List[ApplicationVertex],
